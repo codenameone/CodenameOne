bbe5fcc7df9fdaa3a1fc4479ef1649d3d8bf7e75
Shai Almog <shai@codenameone.com>
2012-12-22 10:52:21 +0000

    Another attempt at fixing issue 441 by blocking the EDT but releasing the iOS thread which might resolve this behavior.

--- Ports/iOSPort/src/com/codename1/impl/ios/IOSImplementation.java
+++ Ports/iOSPort/src/com/codename1/impl/ios/IOSImplementation.java
@@ -79,6 +79,8 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Vector;
 import com.codename1.io.Cookie;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -2289,13 +2291,21 @@ public class IOSImplementation extends CodenameOneImplementation {
 
         public void setFocus(boolean b) {
         }
-
+        
         protected Dimension calcPreferredSize() {
             if(nativePeer == null || nativePeer[0] == 0) {
                 return new Dimension();
             }
             int[] p = new int[2];
+            p[0] = -1;
+            p[1] = -1;
             nativeInstance.calcPreferredSize(nativePeer[0], getDisplayWidth(), getDisplayHeight(), p);
+            while(p[0] < 0 || p[1] < 0) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException ex) {
+                }
+            }
             return new Dimension(p[0], p[1]);
         }
 
