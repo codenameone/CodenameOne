a063926f4ce65226ff6395cd2ddcff7a317a9ad6
Chen Fishbein <chen@codenameone.com>
2012-12-31 09:05:27 +0000

    fixed the scale on the MapComponent

--- CodenameOne/src/com/codename1/maps/MapComponent.java
+++ CodenameOne/src/com/codename1/maps/MapComponent.java
@@ -201,7 +201,7 @@ public class MapComponent extends Container {
                     g.translate(-translateX, -translateY);
                     if (scaleX > 0) {
                         g.translate(-(getWidth() - scaleX) / 2, -(getHeight() - scaleY) / 2);
-                        g.scale(scaleX, scaleY);
+                        g.scale((float)scaleX/(float)getWidth(), (float)scaleY/(float)getHeight());
                         paintmap(g);
                         g.translate((getWidth() - scaleX) / 2, (getHeight() - scaleY) / 2);
                     }else{
