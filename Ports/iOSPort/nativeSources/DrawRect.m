f41e66505e167314f3530c974e929fcda32067ca
Shai Almog <shai@codenameone.com>
2013-01-29 10:02:19 +0000

    Fix for  Issue 486: line border is not displaying properly on android, iphone and ipad

--- Ports/iOSPort/nativeSources/DrawRect.m
+++ Ports/iOSPort/nativeSources/DrawRect.m
@@ -36,10 +36,10 @@
 -(void)execute {
     GlColorFromRGB(color, alpha);
     GLfloat vertexes[] = {
-        x, y,
-        x + width - 1, y,
-        x + width - 1, y + height - 1,
-        x, y + height - 1,
+        x + 1, y + 1,
+        x + width, y + 1,
+        x + width, y + height,
+        x + 1, y + height,
     };
     glEnableClientState(GL_VERTEX_ARRAY);
     glVertexPointer(2, GL_FLOAT, 0, vertexes);
