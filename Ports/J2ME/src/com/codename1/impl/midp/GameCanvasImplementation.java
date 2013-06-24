5fcc1547f84af49ede57530eb86aff1b68d72677
Chen Fishbein <chen@codenameone.com>
2013-06-24 15:09:08 +0000

    added last modified to the impl

--- Ports/J2ME/src/com/codename1/impl/midp/GameCanvasImplementation.java
+++ Ports/J2ME/src/com/codename1/impl/midp/GameCanvasImplementation.java
@@ -2610,6 +2610,22 @@ public class GameCanvasImplementation extends CodenameOneImplementation {
     /**
      * @inheritDoc
      */
+    public long getFileLastModified(String file) {
+        FileConnection fc = null;
+        try {
+            fc = (FileConnection) Connector.open(file, Connector.READ);
+            return fc.lastModified();
+        } catch (IOException err) {
+            err.printStackTrace();
+            return -1;
+        } finally {
+            cleanup(fc);
+        }
+    }
+    
+    /**
+     * @inheritDoc
+     */
     public boolean isDirectory(String file) {
         FileConnection fc = null;
         try {
