8405ff0e61517b37b2f2e64e5d7d1c125bdfb66c
Steve Hannah <steve@weblite.ca>
2014-07-08 23:42:38 +0000

    Removed unused imports added while debugging.

--- Ports/Android/src/com/codename1/impl/android/AndroidGraphics.java
+++ Ports/Android/src/com/codename1/impl/android/AndroidGraphics.java
@@ -41,12 +41,9 @@
  */
 package com.codename1.impl.android;
 
-import java.util.Arrays;
 
 import android.graphics.Bitmap;
-import android.graphics.Camera;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
@@ -56,7 +53,6 @@ import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Region;
 
-import com.codename1.io.Log;
 import com.codename1.ui.Transform;
 
 /**
