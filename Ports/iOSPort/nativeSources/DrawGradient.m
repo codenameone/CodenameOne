4a341cc4f36b552b11f4ce503f0f4aee7c3df372
Steve Hannah <steve@weblite.ca>
2015-03-07 19:08:12 +0000

    Reverted change in gradients that caused a regression.

--- Ports/iOSPort/nativeSources/DrawGradient.m
+++ Ports/iOSPort/nativeSources/DrawGradient.m
@@ -173,17 +173,17 @@ static GLuint getOGLProgram(){
                 float myRadius = MIN(width, height) * relativeSize;
                 CGContextDrawRadialGradient (context, myGradient, myCentrePoint,
                                              0, myCentrePoint, myRadius,
-                                             0);
+                                             kCGGradientDrawsAfterEndLocation);
                 break;
                 
             case GRADIENT_TYPE_HORIZONTAL:
                 CGContextDrawLinearGradient(context, myGradient,
-                                            CGPointMake(0, 0), CGPointMake(0, width), 0);
+                                            CGPointMake(0, 0), CGPointMake(0, width), kCGGradientDrawsBeforeStartLocation | kCGGradientDrawsAfterEndLocation);
                 break;
                 
             case GRADIENT_TYPE_VERTICAL:
                 CGContextDrawLinearGradient(context, myGradient,
-                                            CGPointMake(0, 0), CGPointMake(height, 0), 0);
+                                            CGPointMake(0, 0), CGPointMake(height, 0), kCGGradientDrawsBeforeStartLocation | kCGGradientDrawsAfterEndLocation);
                 break;
         }
         UIGraphicsPopContext();
