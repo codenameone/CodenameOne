e59ebbd91fbc15424f664f4021b47f1f4c96986b
Chen Fishbein <cf27675@gmail.com>
2015-04-06 20:24:33 +0300

    fix for #1418
    Wrong ComboBox height in Android after screen keyboard hiding #1418

--- CodenameOne/src/com/codename1/ui/ComboBox.java
+++ CodenameOne/src/com/codename1/ui/ComboBox.java
@@ -217,7 +217,19 @@ public class ComboBox<T> extends List<T> {
     protected Dialog createPopupDialog(List<T> l) {
         Dialog popupDialog = new Dialog(getUIID() + "Popup", getUIID() + "PopupTitle"){
 
-            protected void autoAdjust(int w, int h) {
+            void sizeChangedInternal(int w, int h) {
+                //if only height changed it's the virtual keyboard, no need to
+                //resize the popup just resize the parent form
+                if(getWidth() == w && getHeight() != h){
+                    Form frm = getPreviousForm();
+                    if(frm != null){
+                        frm.sizeChangedInternal(w, h);
+                    }        
+                    setSize(new Dimension(w, h));
+                    repaint();
+                }else{
+                    dispose();
+                }
             }
             
         };
