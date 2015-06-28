b4bf1a378317b0e9f4e89b856f4175dfe1b12116
codenameone <shai@10.0.1.9>
2015-06-28 11:22:50 +0300

    Updated the service to use the new logging capabilities

--- CodenameOne/src/com/codename1/io/Log.java
+++ CodenameOne/src/com/codename1/io/Log.java
@@ -177,6 +177,45 @@ public class Log {
      * Sends the current log to the cloud regardless of the reporting level
      */
     public static void sendLog() {
+        if(Display.getInstance().getProperty("cloudServerURL", null) != null) {
+            sendLogLegacy();
+            return;
+        }
+        try {
+            // this can cause a crash
+            if(!Display.isInitialized()) {
+                return;
+            }
+            if(!instance.logDirty) {
+                return;
+            }
+            instance.logDirty = false;
+            long devId = getUniqueDeviceId();
+            if(devId < 0) {
+                Dialog.show("Send Log Error", "Device Not Registered: Sending a log from an unregistered device is impossible", "OK", null);
+                return;
+            }
+            ConnectionRequest r = new ConnectionRequest();
+            r.setPost(false);
+            MultipartRequest m = new MultipartRequest();
+            m.setUrl("https://crashreport.codenameone.com/CrashReporterEmail/sendCrashReport");
+            byte[] read = Util.readInputStream(Storage.getInstance().createInputStream("CN1Log__$"));
+            m.addArgument("i", "" + devId);
+            m.addArgument("u",Display.getInstance().getProperty("built_by_user", ""));
+            m.addArgument("p", Display.getInstance().getProperty("package_name", ""));
+            m.addArgument("v", Display.getInstance().getProperty("AppVersion", "0.1"));
+            m.addData("log", read, "text/plain");
+            m.setFailSilently(true);
+            NetworkManager.getInstance().addToQueueAndWait(m);
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Sends the current log to the cloud regardless of the reporting level
+     */
+    private static void sendLogLegacy() {
         try {
             // this can cause a crash
             if(!Display.isInitialized()) {
