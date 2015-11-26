fdd3a2a2255d79bd7efb9f70a45790028d00dda7
Chen Fishbein <cf27675@gmail.com>
2015-11-26 13:20:37 +0200

    added missing docs to the LocationRequest

--- CodenameOne/src/com/codename1/location/LocationRequest.java
+++ CodenameOne/src/com/codename1/location/LocationRequest.java
@@ -1,11 +1,30 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2012, Codename One and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Codename One designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *  
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ * 
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * Please contact Codename One through http://www.codenameone.com/ if you 
+ * need additional information or have any questions.
  */
 package com.codename1.location;
 
 /**
+ * This class is used when requesting to listen to location update.
+ * See {@link com.codename1.location.LocationManager#setLocationListener(com.codename1.location.LocationListener l, com.codename1.location.LocationRequest req) setLocationListener}
  *
  * @author Chen
  */
@@ -30,18 +49,42 @@ public class LocationRequest {
     
     private long interval = 5000;
 
+    /**
+     * Empty Constructor
+     */ 
     public LocationRequest() {
     }
     
+    /**
+     * Simple Constructor
+     * 
+     * @param priority The priority we are interested to listen for location updates.
+     * PRIORITY_HIGH_ACCUARCY, PRIORITY_MEDIUM_ACCUARCY, PRIORITY_LOW_ACCUARCY
+     * High priority means gps locations which is CPU intensive and consumes more battery.
+     * Medium priority is less intensive in terms of battery and might return a 
+     * gps or a network location which is less accurate.
+     * Low priority won't consume the battery and will return a gps location if 
+     * available otherwise the location would be a network location.
+     * 
+     * @param interval time in milliseconds which determines what are the time 
+     * intervals that we would like to get updates from the OS.
+     * This is a request only and might not be respected by the underlying OS
+     */ 
     public LocationRequest(int priority, long interval) {
         this.priority = priority;
         this.interval = interval;
     }
 
+    /**
+     * Gets the request priority
+     */ 
     public int getPriority() {
         return priority;
     }
 
+    /**
+     * Gest the request time interval
+     */ 
     public long getInterval() {
         return interval;
     }
