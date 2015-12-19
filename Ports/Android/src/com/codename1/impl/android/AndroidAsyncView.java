e080821a41d960b6a050383400cfb87732ef1ce0
Steve Hannah <steve@weblite.ca>
2015-12-18 16:10:55 -0800

    Added support for Android async editing.

--- Ports/Android/src/com/codename1/impl/android/AndroidAsyncView.java
+++ Ports/Android/src/com/codename1/impl/android/AndroidAsyncView.java
@@ -111,6 +111,9 @@ public class AndroidAsyncView extends View implements CodenameOneSurface {
         if (paintOnBuffer) {
             cn1View.d(c);
         }
+        if (implementation.isAsyncEditMode() && implementation.isEditingText()) {
+            InPlaceEditView.reLayoutEdit();
+        }
     }
 
     public void setPaintViewOnBuffer(boolean paintViewOnBuffer) {
