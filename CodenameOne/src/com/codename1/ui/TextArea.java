d7875088321c93520707451104b364f1c48f1f19
ddyer0 <ddyer-github@real-me.net>
2016-02-04 11:37:17 -0800

    Revert "avoid array bounds error"
    This reverts commit 8a5c080f75745ee6cc502cb6b1cb3c71601c12a4.

--- CodenameOne/src/com/codename1/ui/TextArea.java
+++ CodenameOne/src/com/codename1/ui/TextArea.java
@@ -787,7 +787,9 @@ public class TextArea extends Component {
     }
 
     private void initRowString() {
-        if(!Display.getInstance().isEdt()) {
+        // this superficially fixes the problem with text
+    	// all displayed on one line.
+    	if(!Display.getInstance().isEdt()) {
             if(rowStrings == null) {
                 rowStrings = new ArrayList();
                 rowStrings.add(getText());
@@ -1007,7 +1009,7 @@ public class TextArea extends Component {
             //then minCharactersInRow
             to=from;//+minCharactersInRow;
         }
-        if(textLength>0 && text[textLength-1] == '\n'){
+        if(textLength>0 && text[textLength -1 ] == '\n'){
             rowStrings.add("");
         }
     }
