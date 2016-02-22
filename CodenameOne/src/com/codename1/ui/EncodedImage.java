codenameone/CodenameOne

commit 230bd23140815ac8e4270dd1217ee399c6186493

Fixed scaledEncoded to support the -1 so we can preserve aspect ratio http://stackoverflow.com/questions/35532917/scaling-encodedimage

CodenameOne/src/com/codename1/ui/EncodedImage.java
@@ -552,15 +552,26 @@ public class EncodedImage extends Image {
     }
 
     /**
-     * Performs scaling using ImageIO to gnerate an encoded Image
-     * @param width the width of the image
-     * @param height the height of the image
+     * Performs scaling using ImageIO to generate an encoded Image
+     * @param width the width of the image, -1 to scale based on height and preserve aspect ratio
+     * @param height the height of the image, -1 to scale based on width and preserve aspect ratio
      * @return new encoded image
      */
     public EncodedImage scaledEncoded(int width, int height) {
         if(width == getWidth() && height == getHeight()) {
             return this;
         }
+        
+        if(width < 0) {
+            float ratio = ((float)height) / ((float)getHeight());
+            width = Math.max(1, (int)(getWidth() * ratio));
+        } else {
+            if(height < 0) {
+                float ratio = ((float)width) / ((float)getWidth());
+                height = Math.max(1, (int)(getWidth() * ratio));
+            }
+        }
+        
         try {
             ImageIO io = ImageIO.getImageIO();
             if(io != null) {

commit 96395d020e90547223d480c82bd768056f20ae0d

Fixed regression in scaledEncoded introduced in https://github.com/codenameone/CodenameOne/commit/230bd23140815ac8e4270dd1217ee399c6186493

CodenameOne/src/com/codename1/ui/EncodedImage.java
@@ -568,7 +568,7 @@ public class EncodedImage extends Image {
         } else {
             if(height < 0) {
                 float ratio = ((float)width) / ((float)getWidth());
-                height = Math.max(1, (int)(getWidth() * ratio));
+                height = Math.max(1, (int)(getHeight() * ratio));
             }
         }
         