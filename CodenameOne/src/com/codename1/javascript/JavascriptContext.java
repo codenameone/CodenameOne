9ca0b23611c9d33d347fdae80496eb24101b9ad6
Steve Hannah <steve@weblite.ca>
2016-03-29 17:05:21 -0700

    Turned debug flag back to off in JavascriptContext.

--- CodenameOne/src/com/codename1/javascript/JavascriptContext.java
+++ CodenameOne/src/com/codename1/javascript/JavascriptContext.java
@@ -61,7 +61,7 @@ public class JavascriptContext  {
     /**
      * Flag to enable/disable logging to a debug log.
      */
-    public static boolean DEBUG=true;
+    public static boolean DEBUG=false;
     
     /**
      * The browser component on which this context operates.
