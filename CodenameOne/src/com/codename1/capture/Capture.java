855c9437ca829b3ca4e6e19933a1ac9a85963e47
Steve Hannah <steve@weblite.ca>
2016-05-09 10:12:35 -0700

    Small change to workaround TeaVM bug that caused IllegalMonitorStateException in CameraDemo.

--- CodenameOne/src/com/codename1/capture/Capture.java
+++ CodenameOne/src/com/codename1/capture/Capture.java
@@ -164,21 +164,25 @@ public class Capture {
         private boolean completed;
         private int targetWidth = -1;
         private int targetHeight = -1;
-        public synchronized void actionPerformed(ActionEvent evt) {
+        public void actionPerformed(ActionEvent evt) {
             if(evt == null) {
                 url = null;
             } else {
                 url = (String)evt.getSource();
             }
             completed = true;
-            notify();
+            synchronized(this) {
+                this.notify();
+            }
         }
 
-        public synchronized void run() {
+        public void run() {
             while(!completed) {
-                try {
-                    wait();
-                } catch (InterruptedException ex) {
+                synchronized(this) {
+                    try {
+                        this.wait();
+                    } catch (InterruptedException ex) {
+                    }
                 }
             }
             if(url == null) {
