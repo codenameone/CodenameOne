ddc6b1a2db8e259604801ba698ca37551057e638
Steve Hannah <steve@weblite.ca>
2016-08-17 14:18:06 -0700

    Added warning in javadocs of InfiniteScrollAdapter that infinite scrolling could be disabled if the target container's removeAll() method is called.

---
+++ Ports/iOSPort/nativeSources/PaintOp.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2012, Codename One and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Codename One designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *  
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ * 
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * Please contact Codename One through http://www.codenameone.com/ if you 
+ * need additional information or have any questions.
+ */
+#import <Foundation/Foundation.h>
+#import "ExecutableOp.h"
+
+
+@interface DrawLine : ExecutableOp {
+    int color;
+    int alpha;
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+}
+
+-(id)initWithArgs:(int)c a:(int)a xpos1:(int)xpos1 ypos1:(int)ypos1 xpos2:(int)xpos2 ypos2:(int)ypos2 ;
+-(void)execute;
+
+@end
