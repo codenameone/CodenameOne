5ec2143a63cf46310b5bbbfcad6d0f267fec0e8a
Steve Hannah <steve@weblite.ca>
2016-10-19 14:51:12 -0700

    Added undocumented -9999 gallery type for use with filechooser lib.

--- Ports/UWP/VSProjectTemplate/UWPApp/src/com/codename1/impl/SilverlightImplementation.cs
+++ Ports/UWP/VSProjectTemplate/UWPApp/src/com/codename1/impl/SilverlightImplementation.cs
@@ -1452,6 +1452,17 @@ namespace com.codename1.impl
                     openPicker.FileTypeFilter.Add(".mov");
                     openPicker.FileTypeFilter.Add(".mpeg");
                     openPicker.FileTypeFilter.Add(".wmv");
+                } else if (type == -9999)
+                {
+                    string typeStr = Display.getInstance().getProperty("windows.openGallery.accept", "*");
+                    string[] parts = typeStr.Split(',');
+                    foreach (string part in parts)
+                    {
+                        openPicker.FileTypeFilter.Add(part);
+                    }
+
+
+                
                 } else
                 {
                     openPicker.FileTypeFilter.Add("*");
@@ -1737,6 +1748,40 @@ namespace com.codename1.impl
         }
 
         public override void execute(string n1) {
+            try
+            {
+                if (exists(n1))
+                {
+                    IStorageItem storageItem = null;
+                    if (isTempFile(n1))
+                    {
+                        storageItem = getTempFile(n1);
+                        
+                    }
+                    else
+                    {
+                        StorageFolder store = getStore(n1);
+                        string f = nativePathStore(n1);
+                        storageItem = store.TryGetItemAsync(f).AsTask().GetAwaiter().GetResult();
+                        
+
+                    }
+                    if (storageItem != null && storageItem is StorageFile)
+                    {
+                        using (AutoResetEvent are = new AutoResetEvent(false))
+                        {
+                            dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
+                                var result = Windows.System.Launcher.LaunchFileAsync((StorageFile)storageItem);
+                                are.Set();
+                            }).AsTask().GetAwaiter().GetResult();
+                            are.WaitOne();
+                        }
+                        return;
+                    }
+                    
+                }
+            }
+            catch (Exception ex) { }
             var uri = new Uri(n1, UriKind.RelativeOrAbsolute);
             using (AutoResetEvent are = new AutoResetEvent(false)) {
                 dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
