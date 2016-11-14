codenameone/CodenameOne

commit 05b7d1cff7f6811553f7d06c1eaba2cd4938a7b2

Fixed issue with multiple unsequenced modifications to SP  on iOS when allocating 2D arrays.  https://github.com/codenameone/CodenameOne/issues/1910

vm/ByteCodeTranslator/src/com/codename1/tools/translator/bytecodes/MultiArray.java
@@ -93,13 +93,13 @@ public class MultiArray extends Instruction {
         }*/
         switch(actualDim) {
             case 2:
-                b.append("    PUSH_OBJ(alloc2DArray(threadStateData, (*(--SP)).data.i, ");
+                b.append("    PUSH_OBJ(alloc2DArray(threadStateData, (*(SP-1)).data.i, ");
                 switch(dims) {
                     case 1:
                         b.append("-1");
                         break;
                     case 2:
-                        b.append("(*(--SP)).data.i");
+                        b.append("(*(SP-2)).data.i");
                         break;
                 }
                 b.append(", &class_array2__");
@@ -112,7 +112,7 @@ public class MultiArray extends Instruction {
                 } else {
                     b.append("JAVA_OBJECT");
                 }
-                b.append("))); /* MULTIANEWARRAY */\n");
+                b.append("))); SP -= ").append(dims).append("; /* MULTIANEWARRAY */\n");
                 break;
                 
             case 3:

commit 5ade5dfabe2e202862bf9c4f8a18e15dea84be04

Fixed regression with 2D arrays caused by https://github.com/codenameone/CodenameOne/commit/05b7d1cff7f6811553f7d06c1eaba2cd4938a7b2
Ref: https://github.com/codenameone/CodenameOne/issues/1910
https://github.com/codenameone/CodenameOne/issues/1960

vm/ByteCodeTranslator/src/com/codename1/tools/translator/bytecodes/MultiArray.java
@@ -93,13 +93,13 @@ public class MultiArray extends Instruction {
         }*/
         switch(actualDim) {
             case 2:
-                b.append("    PUSH_OBJ(alloc2DArray(threadStateData, (*(SP-1)).data.i, ");
+                b.append("    SP -= ").append(dims).append("; PUSH_OBJ(alloc2DArray(threadStateData, ");
                 switch(dims) {
                     case 1:
-                        b.append("-1");
+                        b.append("(*SP).data.i, -1");
                         break;
                     case 2:
-                        b.append("(*(SP-2)).data.i");
+                        b.append("(*(SP+1)).data.i, (*SP).data.i");
                         break;
                 }
                 b.append(", &class_array2__");
@@ -112,7 +112,7 @@ public class MultiArray extends Instruction {
                 } else {
                     b.append("JAVA_OBJECT");
                 }
-                b.append("))); SP -= ").append(dims).append("; /* MULTIANEWARRAY */\n");
+                b.append("))); /* MULTIANEWARRAY */\n");
                 break;
                 
             case 3: