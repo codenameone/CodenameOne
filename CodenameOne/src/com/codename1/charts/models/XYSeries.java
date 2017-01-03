codenameone/CodenameOne

commit 741aaaeacaa73736649a54e58c205b69c0f897d3

Fixed issue with xy charts not rendering y=0.  Turns out that (Double.MIN_VALUE==0) == true.  https://github.com/codenameone/CodenameOne/issues/1845

CodenameOne/src/com/codename1/charts/util/MathHelper.java
@@ -26,7 +26,7 @@ import com.codename1.util.MathUtil;
  */
 public class MathHelper {
   /** A value that is used a null value. */
-  public static final double NULL_VALUE = Double.MIN_VALUE;
+  public static final double NULL_VALUE = -Double.MAX_VALUE+1;
   /**
    * A number formatter to be used to make sure we have a maximum number of
    * fraction digits in the labels.

commit fbea404cf5cd63af055ac8eb17adea1a9b3becb9

Fixed issue with conflating NULL_VALUE with min and max double values in charts package.  The use of a NULL_VALUE double constant throughout is problematic since comparing doubles with == is not a good idea.  But this change seems to have fixed the regression caused by https://github.com/codenameone/CodenameOne/commit/741aaaeacaa73736649a54e58c205b69c0f897d3 This change is to fix https://github.com/codenameone/CodenameOne/issues/1997

CodenameOne/src/com/codename1/charts/models/XYSeries.java
@@ -39,11 +39,11 @@ public class XYSeries{
   /** The minimum value for the X axis. */
   private double mMinX = MathHelper.NULL_VALUE;
   /** The maximum value for the X axis. */
-  private double mMaxX = -MathHelper.NULL_VALUE;
+  private double mMaxX = MathHelper.NULL_VALUE;
   /** The minimum value for the Y axis. */
   private double mMinY = MathHelper.NULL_VALUE;
   /** The maximum value for the Y axis. */
-  private double mMaxY = -MathHelper.NULL_VALUE;
+  private double mMaxY = MathHelper.NULL_VALUE;
   /** The scale number for this series. */
   private final int mScaleNumber;
   /** Contains the annotations. */
@@ -81,9 +81,9 @@ public class XYSeries{
    */
   private void initRange() {
     mMinX = MathHelper.NULL_VALUE;
-    mMaxX = -MathHelper.NULL_VALUE;
+    mMaxX = MathHelper.NULL_VALUE;
     mMinY = MathHelper.NULL_VALUE;
-    mMaxY = -MathHelper.NULL_VALUE;
+    mMaxY = MathHelper.NULL_VALUE;
     int length = getItemCount();
     for (int k = 0; k < length; k++) {
       double x = getX(k);
@@ -101,10 +101,10 @@ public class XYSeries{
    * @param y the new y value
    */
   private void updateRange(double x, double y) {
-    mMinX = Math.min(mMinX, x);
-    mMaxX = Math.max(mMaxX, x);
-    mMinY = Math.min(mMinY, y);
-    mMaxY = Math.max(mMaxY, y);
+    mMinX = mMinX == MathHelper.NULL_VALUE ? x : Math.min(mMinX, x);
+    mMaxX = mMaxX == MathHelper.NULL_VALUE ? x : Math.max(mMaxX, x);
+    mMinY = mMinY == MathHelper.NULL_VALUE ? y : Math.min(mMinY, y);
+    mMaxY = mMaxY == MathHelper.NULL_VALUE ? y : Math.max(mMaxY, y);
     
    
   }

CodenameOne/src/com/codename1/charts/models/XYValueSeries.java
@@ -30,7 +30,7 @@ public class XYValueSeries extends XYSeries {
   /** The minimum value. */
   private double mMinValue = MathHelper.NULL_VALUE;
   /** The maximum value. */
-  private double mMaxValue = -MathHelper.NULL_VALUE;
+  private double mMaxValue = MathHelper.NULL_VALUE;
 
   /**
    * Builds a new XY value series.
@@ -72,8 +72,8 @@ public class XYValueSeries extends XYSeries {
    * @param value the new value
    */
   private void updateRange(double value) {
-    mMinValue = Math.min(mMinValue, value);
-    mMaxValue = Math.max(mMaxValue, value);
+    mMinValue = mMinValue == MathHelper.NULL_VALUE ? value : Math.min(mMinValue, value);
+    mMaxValue = mMaxValue == MathHelper.NULL_VALUE ? value : Math.max(mMaxValue, value);
   }
 
   /**

CodenameOne/src/com/codename1/charts/renderers/DialRenderer.java
@@ -32,7 +32,7 @@ public class DialRenderer extends DefaultRenderer {
   /** The start value in dial range. */
   private double mMinValue = MathHelper.NULL_VALUE;
   /** The end value in dial range. */
-  private double mMaxValue = -MathHelper.NULL_VALUE;
+  private double mMaxValue = MathHelper.NULL_VALUE;
   /** The spacing for the minor ticks. */
   private double mMinorTickSpacing = MathHelper.NULL_VALUE;
   /** The spacing for the major ticks. */
@@ -131,7 +131,7 @@ public class DialRenderer extends DefaultRenderer {
    * @return the maximum dial was set or not
    */
   public boolean isMaxValueSet() {
-    return mMaxValue != -MathHelper.NULL_VALUE;
+    return mMaxValue != MathHelper.NULL_VALUE;
   }
 
   /**

CodenameOne/src/com/codename1/charts/renderers/XYMultipleSeriesRenderer.java
@@ -165,9 +165,9 @@ public class XYMultipleSeriesRenderer extends DefaultRenderer {
 
   public void initAxesRangeForScale(int i) {
     mMinX[i] = MathHelper.NULL_VALUE;
-    mMaxX[i] = -MathHelper.NULL_VALUE;
+    mMaxX[i] = MathHelper.NULL_VALUE;
     mMinY[i] = MathHelper.NULL_VALUE;
-    mMaxY[i] = -MathHelper.NULL_VALUE;
+    mMaxY[i] = MathHelper.NULL_VALUE;
     double[] range = new double[] { mMinX[i], mMaxX[i], mMinY[i], mMaxY[i] };
     initialRange.put(i, range);
     mYTitle[i] = "";
@@ -455,7 +455,7 @@ public class XYMultipleSeriesRenderer extends DefaultRenderer {
    * @return the maxX was set or not
    */
   public boolean isMaxXSet(int scale) {
-    return mMaxX[scale] != -MathHelper.NULL_VALUE;
+    return mMaxX[scale] != MathHelper.NULL_VALUE;
   }
 
   /**
@@ -521,7 +521,7 @@ public class XYMultipleSeriesRenderer extends DefaultRenderer {
    * @return the maxY was set or not
    */
   public boolean isMaxYSet(int scale) {
-    return mMaxY[scale] != -MathHelper.NULL_VALUE;
+    return mMaxY[scale] != MathHelper.NULL_VALUE;
   }
 
   /**

CodenameOne/src/com/codename1/charts/util/MathHelper.java
@@ -52,8 +52,8 @@ public class MathHelper {
     int length = values.size();
     for (int i = 1; i < length; i++) {
       double value = values.get(i);
-      min = Math.min(min, value);
-      max = Math.max(max, value);
+      min = min == NULL_VALUE ? value : Math.min(min, value);
+      max = max == NULL_VALUE ? value : Math.max(max, value);
     }
     return new double[] { min, max };
   }

CodenameOne/src/com/codename1/charts/views/DialChart.java
@@ -99,10 +99,10 @@ public class DialChart extends RoundChart {
       for (int i = 0; i < count; i++) {
         double value = mDataset.getValue(i);
         if (!mRenderer.isMinValueSet()) {
-          min = Math.min(min, value);
+          min = min == MathHelper.NULL_VALUE ? value : Math.min(min, value);
         }
         if (!mRenderer.isMaxValueSet()) {
-          max = Math.max(max, value);
+          max = max == MathHelper.NULL_VALUE ? value : Math.max(max, value);
         }
       }
     }

CodenameOne/src/com/codename1/charts/views/XYChart.java
@@ -195,23 +195,44 @@ public abstract class XYChart extends AbstractChart {
       }
       if (!isMinXSet[scale]) {
         double minimumX = series.getMinX();
-        minX[scale] = Math.min(minX[scale], minimumX);
-        mCalcRange.get(scale)[0] = minX[scale];
+        if (minimumX != MathHelper.NULL_VALUE) {
+            minX[scale] = minX[scale] == MathHelper.NULL_VALUE ? minimumX : Math.min(minX[scale], minimumX);
+            mCalcRange.get(scale)[0] = minX[scale];
+        } else {
+            minX[scale] = 0;
+            mCalcRange.get(scale)[0] = 0;
+        }
       }
       if (!isMaxXSet[scale]) {
         double maximumX = series.getMaxX();
-        maxX[scale] = Math.max(maxX[scale], maximumX);
-        mCalcRange.get(scale)[1] = maxX[scale];
+        if (maximumX != MathHelper.NULL_VALUE) {
+            maxX[scale] = maxX[scale] == MathHelper.NULL_VALUE ? maximumX : Math.max(maxX[scale], maximumX);
+            mCalcRange.get(scale)[1] = maxX[scale];
+        } else {
+            maxX[scale] = minX[scale] + 1;
+            mCalcRange.get(scale)[1] = maxX[scale];
+        }
       }
       if (!isMinYSet[scale]) {
         double minimumY = series.getMinY();
-        minY[scale] = Math.min(minY[scale], (float) minimumY);
-        mCalcRange.get(scale)[2] = minY[scale];
+        if (minimumY != MathHelper.NULL_VALUE) {
+            minY[scale] = minY[scale] == MathHelper.NULL_VALUE ? minimumY : Math.min(minY[scale], (float) minimumY);
+            mCalcRange.get(scale)[2] = minY[scale];
+        } else {
+            minY[scale] = 0;
+            mCalcRange.get(scale)[2] = 0;
+        }
       }
       if (!isMaxYSet[scale]) {
         double maximumY = series.getMaxY();
-        maxY[scale] = Math.max(maxY[scale], (float) maximumY);
-        mCalcRange.get(scale)[3] = maxY[scale];
+        if (maximumY != MathHelper.NULL_VALUE) {
+            maxY[scale] = maxY[scale] == MathHelper.NULL_VALUE ? maximumY : Math.max(maxY[scale], (float) maximumY);
+            mCalcRange.get(scale)[3] = maxY[scale];
+        } else {
+            maxY[scale] = minY[scale] + 1;
+            mCalcRange.get(scale)[3] = maxY[scale];
+        }
+        
       }
     }
     for (int i = 0; i < maxScaleNumber; i++) {