d9d82c9cb7bbd6636d4a554a3e96217b72a61b67
Steve Hannah <steve@weblite.ca>
2017-01-12 10:12:09 -0800

    Fixed issue with crashing if the app is paused while editing a text field in a dialog.  http://stackoverflow.com/questions/41383494/codename-one-app-crashes-if-minimised-while-virtual-keyboard-is-showing/41413872

--- Ports/Android/src/com/codename1/impl/android/CodenameOneActivity.java
+++ Ports/Android/src/com/codename1/impl/android/CodenameOneActivity.java
@@ -401,6 +401,9 @@ public class CodenameOneActivity extends Activity {
 
     @Override
     protected void onPause() {
+        if (InPlaceEditView.isEditing()) {
+            AndroidImplementation.stopEditing(true);
+        }
         super.onPause();
         AndroidNativeUtil.onPause();
     }
