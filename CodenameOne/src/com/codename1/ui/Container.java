50b2c008dc779c5b9b686ee788e6a2aa3d8dd4a0
Steve Hannah <steve@weblite.ca>
2017-03-06 14:30:42 -0800

    Added Container.createAnimateUnlayout() method.

--- CodenameOne/src/com/codename1/ui/Container.java
+++ CodenameOne/src/com/codename1/ui/Container.java
@@ -2669,7 +2669,7 @@ public class Container extends Component implements Iterable<Component>{
      * @param callback if not null will be invoked when unlayouting is complete
      */
     public void animateUnlayout(final int duration, int opacity, Runnable callback) {
-        animateUnlayout(duration, false, opacity, callback);
+        animateUnlayout(duration, false, opacity, callback, true);
     }
     
     /**
@@ -2682,15 +2682,28 @@ public class Container extends Component implements Iterable<Component>{
      * @param opacity the opacity to which the layout will reach, allows fading out the components
      */
     public void animateUnlayoutAndWait(final int duration, int opacity) {
-        animateUnlayout(duration, true, opacity, null);
+        animateUnlayout(duration, true, opacity, null, true);
     }
 
+    /**
+     * <p>This method is the exact reverse of createAnimateLayout, when animation is completed it leaves the container in 
+     * an invalid state. It is useful to invoke this in order to remove a component, transition to a
+     * different form or provide some other interaction. E.g.:</p>
+     * 
+     * @param duration the duration of the animation
+     * @param opacity the opacity to which the layout will reach, allows fading out the components
+     * @return the animation object that should be added to the animation manager
+     */
+    public ComponentAnimation createAnimateUnlayout(int duration, int opacity, Runnable callback) {
+        return animateUnlayout(duration, false, opacity, callback, false);
+    }
+    
     /**
      * Animates a pending layout into place, this effectively replaces revalidate with a more visual form of animation
      *
      * @param duration the duration in milliseconds for the animation
      */
-    private void animateUnlayout(final int duration, boolean wait, int opacity, Runnable callback) {
+    private ComponentAnimation animateUnlayout(final int duration, boolean wait, int opacity, Runnable callback, boolean add) {
         setShouldCalcPreferredSize(true);
         enableLayoutOnPaint = false;
         final int componentCount = getComponentCount();
@@ -2726,15 +2739,18 @@ public class Container extends Component implements Iterable<Component>{
         });
         setAnimOpacity(255, opacity, a, componentCount, duration);
         a.dontRevalidate = true;
-        if(wait) {
-            getAnimationManager().addAnimationAndBlock(a);
-        } else {
-            if(callback != null) {
-                getAnimationManager().addAnimation(a, callback);
+        if (add) {
+            if(wait) {
+                getAnimationManager().addAnimationAndBlock(a);
             } else {
-                getAnimationManager().addAnimation(a);
+                if(callback != null) {
+                    getAnimationManager().addAnimation(a, callback);
+                } else {
+                    getAnimationManager().addAnimation(a);
+                }
             }
         }
+        return a;
     }
     
     /**
