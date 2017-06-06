9ebcd6c7b22006a6e2a326cd415ded0ebd587eb5
Steve Hannah <steve@weblite.ca>
2017-06-06 11:08:47 -0700

    Replaced NSLog calls with CN1Log which is a macro that uses printf instead.  This works around an issue with iOS 10 not showing NSLog output in device log anymore.

--- Ports/iOSPort/nativeSources/CodenameOne_GLViewController.h
+++ Ports/iOSPort/nativeSources/CodenameOne_GLViewController.h
@@ -66,7 +66,9 @@
 //#define BACKGROUND_LOCATION_ENABLED
 #define CN1_REQUEST_LOCATION_AUTH requestWhenInUseAuthorization
 
-#define IOS8_LOCATION_WARNING NSLog(@"As of iOS8, location services requires the ios.locationUsageDescription build hint to be set.");
+#define CN1Log(str,...) printf([[NSString stringWithFormat:str,##__VA_ARGS__] UTF8String])
+
+#define IOS8_LOCATION_WARNING CN1Log(@"As of iOS8, location services requires the ios.locationUsageDescription build hint to be set.");
 //#define CN1_ENABLE_BACKGROUND_LOCATION 1
 
 //#define INCLUDE_MOPUB
