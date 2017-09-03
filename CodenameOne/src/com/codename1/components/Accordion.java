431ed2644246fb9a3e11b828a399ce451a40d1a2
Diamond <diamondobama@gmail.com>
2017-09-03 06:07:35 +0200

    Improved Javadoc for setting accordion icons

--- CodenameOne/src/com/codename1/components/Accordion.java
+++ CodenameOne/src/com/codename1/components/Accordion.java
@@ -193,24 +193,24 @@ public class Accordion extends Container {
     }
 
     /**
-     * Sets the closed icon
-     * @param closeIcon the close icon
+     * Sets the closed icon using material image
+     * @param closeIcon the close icon (e.g. {@code FontImage.MATERIAL_KEYBOARD_ARROW_DOWN})
      */ 
     public void setCloseIcon(char closeIcon) {
         this.closeIcon = FontImage.createMaterial(closeIcon, UIManager.getInstance().getComponentStyle("AccordionArrow"));
     }
 
     /**
-     * Sets the open icon
-     * @param openIcon the open icon
+     * Sets the open icon using material image
+     * @param openIcon the open icon (e.g. {@code FontImage.MATERIAL_KEYBOARD_ARROW_RIGHT})
      */ 
     public void setOpenIcon(char openIcon) {
         this.openIcon = FontImage.createMaterial(openIcon, UIManager.getInstance().getComponentStyle("AccordionArrow"));
     }
     
     /**
-     * Sets the closed icon customized
-     * @param closeIcon the close icon
+     * Sets the closed icon using material image with a custom uiid
+     * @param closeIcon the close icon (e.g. {@code FontImage.MATERIAL_KEYBOARD_ARROW_DOWN})
      * @param uiid to custom icon from res file
      */ 
     public void setCloseIcon(char closeIcon, String uiid) {
@@ -218,8 +218,8 @@ public class Accordion extends Container {
     }
 
     /**
-     * Sets the open icon customized
-     * @param openIcon the open icon
+     * Sets the open icon using material image with a custom uiid
+     * @param openIcon the open icon (e.g. {@code FontImage.MATERIAL_KEYBOARD_ARROW_RIGHT})
      * @param uiid to custom icon from res file
      */ 
     public void setOpenIcon(char openIcon, String uiid) {
