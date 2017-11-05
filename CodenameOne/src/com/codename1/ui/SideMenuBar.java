119120cf98796bee2a8da3f4ac758f167367b0f2
Shai Almog <shai@codenameone.com>
2017-11-05 06:57:31 +0200

    Fixed regression for old SideMenuBar code

--- CodenameOne/src/com/codename1/ui/SideMenuBar.java
+++ CodenameOne/src/com/codename1/ui/SideMenuBar.java
@@ -153,7 +153,7 @@ public class SideMenuBar extends MenuBar {
      * @param callback will be invoked when the menu is actually closed
      */
     public static void closeCurrentMenu(final Runnable callback) {
-        if(Toolbar.isOnTopSideMenu()) {
+        if(Toolbar.isOnTopSideMenu() && Display.getInstance().getCommandBehavior() != Display.COMMAND_BEHAVIOR_SIDE_NAVIGATION) {
             Display.getInstance().getCurrent().getToolbar().closeSideMenu();
             callback.run();
             return;
@@ -700,7 +700,7 @@ public class SideMenuBar extends MenuBar {
      * Closes the menu if it is currently open
      */
     public void closeMenu() {
-        if(Toolbar.isOnTopSideMenu() && Display.getInstance().getCurrent().getToolbar() != null) {
+        if(Toolbar.isOnTopSideMenu() && Display.getInstance().getCurrent().getToolbar() != null  && Display.getInstance().getCommandBehavior() != Display.COMMAND_BEHAVIOR_SIDE_NAVIGATION) {
             Display.getInstance().getCurrent().getToolbar().closeSideMenu();
             return;
         }
@@ -1774,7 +1774,7 @@ public class SideMenuBar extends MenuBar {
         }
 
         public void actionPerformed(final ActionEvent evt) {
-            if(Toolbar.isOnTopSideMenu()) {
+            if(Toolbar.isOnTopSideMenu() && Display.getInstance().getCommandBehavior() != Display.COMMAND_BEHAVIOR_SIDE_NAVIGATION) {
                 Display.getInstance().getCurrent().getToolbar().closeSideMenu();
                 cmd.actionPerformed(evt);
                 return;
