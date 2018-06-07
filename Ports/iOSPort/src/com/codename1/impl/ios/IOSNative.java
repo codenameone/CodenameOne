944d388da0e74a3fe343a81384e28ab01649d3ac
ramsestom <ramsestom@github.com>
2018-06-07 22:35:27 +0200

    Refactor getDeviceDensity() and introduce getDeviceDPI() and getFontScale()
    # Conflicts:
    #	Ports/JavaSE/src/com/codename1/impl/javase/JavaSEPort.java

--- Ports/iOSPort/src/com/codename1/impl/ios/IOSNative.java
+++ Ports/iOSPort/src/com/codename1/impl/ios/IOSNative.java
@@ -129,6 +129,9 @@ public final class IOSNative {
     native boolean isTablet();
     native boolean isIOS7();
     
+    native float fontScale();
+    native String getModelID();
+    
     native void setImageName(long nativeImage, String name);
     
     native boolean canExecute(String url);
