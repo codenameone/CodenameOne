codenameone/CodenameOne

commit 5d1824db25b2af432dace70c57a394c299850c60

Improved PushNotificationService to support changes on Android API 26+ that require the use of a NotificationChannel.  This change is only activated by the build server when targeting sdk 26 or higher.  In addition, this change only applies to devices that don't have Google Play.  The rest of the fix for push notifications in API 26+ (e.g. for Google Play devices) is part of the build server, as it generates the PushReceiver class which is used by google play devices.

Ports/Android/src/com/codename1/impl/android/PushNotificationService.java
@@ -101,7 +101,9 @@ public abstract class PushNotificationService extends Service implements PushCal
             NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
             Intent newIntent = new Intent(this, getStubClass());
             PendingIntent contentIntent = PendingIntent.getActivity(this, 0, newIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-            
+
+
+
             Notification.Builder builder = new Notification.Builder(this)
                     .setContentIntent(contentIntent)
                     .setSmallIcon(android.R.drawable.stat_notify_sync)
@@ -109,7 +111,57 @@ public abstract class PushNotificationService extends Service implements PushCal
                     .setAutoCancel(true)
                     .setWhen(System.currentTimeMillis())
                     .setContentTitle(value)
+
                     .setDefaults(Notification.DEFAULT_ALL);
+
+
+
+
+            // The following section is commented out so that builds against SDKs below 26
+            // won't fail.
+            /*<SDK26>
+            if(android.os.Build.VERSION.SDK_INT >= 21){
+                builder.setCategory("Notification");
+            }
+            if (android.os.Build.VERSION.SDK_INT >= 26) {
+                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                String id = Display.getInstance().getProperty("android.NotificationChannel.id", "cn1-channel");
+
+                CharSequence name = Display.getInstance().getProperty("android.NotificationChannel.name", "Notifications");
+
+                String description = Display.getInstance().getProperty("android.NotificationChannel.description", "Remote notifications");
+
+                int importance = Integer.parseInt(Display.getInstance().getProperty("android.NotificationChannel.importance", ""+NotificationManager.IMPORTANCE_LOW));
+
+                android.app.NotificationChannel mChannel = new android.app.NotificationChannel(id, name,importance);
+
+                mChannel.setDescription(description);
+
+                mChannel.enableLights(Boolean.parseBoolean(Display.getInstance().getProperty("android.NotificationChannel.enableLights", "true")));
+
+                mChannel.setLightColor(Integer.parseInt(Display.getInstance().getProperty("android.NotificationChannel.lightColor", ""+android.graphics.Color.RED)));
+
+                mChannel.enableVibration(Boolean.parseBoolean(Display.getInstance().getProperty("android.NotificationChannel.enableVibration", "false")));
+                String vibrationPatternStr = Display.getInstance().getProperty("android.NotificationChannel.vibrationPattern", null);
+                if (vibrationPatternStr != null) {
+                    String[] parts = vibrationPatternStr.split(",");
+                    int len = parts.length;
+                    long[] pattern = new long[len];
+                    for (int i=0; i<len; i++) {
+                        pattern[i] = Long.parseLong(parts[i].trim());
+                    }
+                    mChannel.setVibrationPattern(pattern);
+                }
+
+
+
+                mNotificationManager.createNotificationChannel(mChannel);
+                System.out.println("Setting push channel to "+id);
+                builder.setChannelId(id);
+            }
+            </SDK26>*/
+
             Notification notif = builder.build();
             nm.notify((int)System.currentTimeMillis(), notif);
         }

commit eb681d212f7a89438c53223460f433c72316c6a5

Fixed regression in Android push notifications when app is not running.  Regression was introduced in https://github.com/codenameone/CodenameOne/commit/5d1824db25b2af432dace70c57a394c299850c60 .  In order to fix this, we had to move some some display properties regarding notification channels into build hints.  These properties haven't been documented yet. This also includeds fix for Display.execute(filePath) in SDK 27.  https://github.com/codenameone/CodenameOne/issues/2461 Both of these fixes require forthcoming patches to the build server also.

Ports/Android/src/com/codename1/impl/android/AndroidImplementation.java
@@ -2545,6 +2545,44 @@ public class AndroidImplementation extends CodenameOneImplementation implements 
         return type;
     }
 
+    public static void copy(File src, File dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        try {
+            OutputStream out = new FileOutputStream(dst);
+            try {
+                // Transfer bytes from in to out
+                byte[] buf = new byte[8096];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+            } finally {
+                out.close();
+            }
+        } finally {
+            in.close();
+        }
+    }
+
+    private static File makeTempCacheCopy(File file) throws IOException {
+        File cacheDir = new File(getContext().getCacheDir(), "intent_files");
+
+        // Create the storage directory if it does not exist
+        if (!cacheDir.exists()) {
+            if (!cacheDir.mkdirs()) {
+                Log.d(Display.getInstance().getProperty("AppName", "CodenameOne"), "failed to create directory");
+                return null;
+            }
+        }
+
+        File copy = new File(cacheDir, "tmp-"+System.currentTimeMillis()+file.getName());
+        copy(file, copy);
+        return copy;
+
+    }
+
+
+
     private Intent createIntentForURL(String url) {
         Intent intent;
         Uri uri;
@@ -2557,13 +2595,58 @@ public class AndroidImplementation extends CodenameOneImplementation implements 
                         return null;
                     }
                 }
-                url = fixAttachmentPath(url);
                 intent = new Intent();
                 intent.setAction(Intent.ACTION_VIEW);
                 if (url.startsWith("/")) {
-                    uri = Uri.fromFile(new File(url));
+                    File f = new File(url);
+                    Uri furi = null;
+                    try {
+                        furi = FileProvider.getUriForFile(getContext(), getContext().getPackageName()+".provider", f);
+                    } catch (Exception ex) {
+                        f = makeTempCacheCopy(f);
+                        furi = FileProvider.getUriForFile(getContext(), getContext().getPackageName()+".provider", f);
+                    }
+        
+        
+                    if (Build.VERSION.SDK_INT < 21) {
+                        List<ResolveInfo> resInfoList = getContext().getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+                        for (ResolveInfo resolveInfo : resInfoList) {
+                            String packageName = resolveInfo.activityInfo.packageName;
+                            getContext().grantUriPermission(packageName, furi, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                        }
+                    }
+                    
+                    uri = furi;
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 }else{
-                    uri = Uri.parse(url);
+
+                    if (url.startsWith("file:")) {
+                        File f = new File(removeFilePrefix(url));
+                        System.out.println("File size: "+f.length());
+
+                        Uri furi = null;
+                        try {
+                            furi = FileProvider.getUriForFile(getContext(), getContext().getPackageName()+".provider", f);
+                        } catch (Exception ex) {
+                            f = makeTempCacheCopy(f);
+                            furi = FileProvider.getUriForFile(getContext(), getContext().getPackageName()+".provider", f);
+                        }
+
+
+                        if (Build.VERSION.SDK_INT < 21) {
+                            List<ResolveInfo> resInfoList = getContext().getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+                            for (ResolveInfo resolveInfo : resInfoList) {
+                                String packageName = resolveInfo.activityInfo.packageName;
+                                getContext().grantUriPermission(packageName, furi, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                            }
+                        }
+                        uri = furi;
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+
+                    } else {
+                        uri = Uri.parse(url);
+                    }
                 }
                 String mimeType = getMimeType(url);
                 if(mimeType != null){

Ports/Android/src/com/codename1/impl/android/PushNotificationService.java
@@ -42,6 +42,8 @@ import com.codename1.ui.Display;
  */
 public abstract class PushNotificationService extends Service implements PushCallback {
 
+    private java.util.Map<String,String> properties = new java.util.HashMap<String,String>();
+    
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -49,6 +51,19 @@ public abstract class PushNotificationService extends Service implements PushCal
     
     @Override
     public void onCreate() {
+    }
+    
+    private String getProperty(String propertyName, String defaultValue) {
+        String val = properties.get(propertyName);
+        if (val == null) {
+            return defaultValue;
+        }
+        return val;
+    }
+    
+    public void setProperty(String propertyName, String val) {
+        properties.put(propertyName, val);
+        
     }
     
     public static void startServiceIfRequired(Class service, Context ctx) {
@@ -126,24 +141,24 @@ public abstract class PushNotificationService extends Service implements PushCal
             if (android.os.Build.VERSION.SDK_INT >= 26) {
                 NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-                String id = Display.getInstance().getProperty("android.NotificationChannel.id", "cn1-channel");
+                String id = getProperty("android.NotificationChannel.id", "cn1-channel");
 
-                CharSequence name = Display.getInstance().getProperty("android.NotificationChannel.name", "Notifications");
+                CharSequence name = getProperty("android.NotificationChannel.name", "Notifications");
 
-                String description = Display.getInstance().getProperty("android.NotificationChannel.description", "Remote notifications");
+                String description = getProperty("android.NotificationChannel.description", "Remote notifications");
 
-                int importance = Integer.parseInt(Display.getInstance().getProperty("android.NotificationChannel.importance", ""+NotificationManager.IMPORTANCE_LOW));
+                int importance = Integer.parseInt(getProperty("android.NotificationChannel.importance", ""+NotificationManager.IMPORTANCE_LOW));
 
                 android.app.NotificationChannel mChannel = new android.app.NotificationChannel(id, name,importance);
 
                 mChannel.setDescription(description);
 
-                mChannel.enableLights(Boolean.parseBoolean(Display.getInstance().getProperty("android.NotificationChannel.enableLights", "true")));
+                mChannel.enableLights(Boolean.parseBoolean(getProperty("android.NotificationChannel.enableLights", "true")));
 
-                mChannel.setLightColor(Integer.parseInt(Display.getInstance().getProperty("android.NotificationChannel.lightColor", ""+android.graphics.Color.RED)));
+                mChannel.setLightColor(Integer.parseInt(getProperty("android.NotificationChannel.lightColor", ""+android.graphics.Color.RED)));
 
-                mChannel.enableVibration(Boolean.parseBoolean(Display.getInstance().getProperty("android.NotificationChannel.enableVibration", "false")));
-                String vibrationPatternStr = Display.getInstance().getProperty("android.NotificationChannel.vibrationPattern", null);
+                mChannel.enableVibration(Boolean.parseBoolean(getProperty("android.NotificationChannel.enableVibration", "false")));
+                String vibrationPatternStr = getProperty("android.NotificationChannel.vibrationPattern", null);
                 if (vibrationPatternStr != null) {
                     String[] parts = vibrationPatternStr.split(",");
                     int len = parts.length;