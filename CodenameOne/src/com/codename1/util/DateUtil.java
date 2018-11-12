19de991b5d88ce0f2f2760ee7c38d98b861f96fb
Steve Hannah <steve@weblite.ca>
2018-11-12 09:33:47 -0800

    Added getTimeAgo() method to DateUtil to make it easier to view dates in a format like 'Just now', '5 minutes ago', 'yesterday', etc...

--- CodenameOne/src/com/codename1/util/DateUtil.java
+++ CodenameOne/src/com/codename1/util/DateUtil.java
@@ -82,5 +82,85 @@ public class DateUtil {
         return tz.useDaylightTime() && getOffset(date.getTime()) != tz.getRawOffset();
     }
     
+    /**
+     * Gets the date in "time ago" format.  E.g. "Just now", or "1 day ago", etc..
+     * @param date The date
+     * @return String representing how long ago from now the given date is.
+     * @since 6.0
+     */
+    public String getTimeAgo(Date date) {
+        if (date == null) {
+            return "N/A";
+        }
+        long time_ago = date.getTime() / 1000l;
+        
+        long cur_time =  new Date().getTime() / 1000l;
+        long time_elapsed = cur_time - time_ago;
+        long seconds = time_elapsed;
+        // Seconds
+        if (seconds <= 60) {
+            return "Just now";
+        } //Minutes
+        else {
+            int minutes = Math.round(time_elapsed / 60);
+
+            if (minutes <= 60) {
+                if (minutes == 1) {
+                    return "a minute ago";
+                } else {
+                    return minutes + " minutes ago";
+                }
+            } //Hours
+            else {
+                int hours = Math.round(time_elapsed / 3600);
+                if (hours <= 24) {
+                    if (hours == 1) {
+                        return "An hour ago";
+                    } else {
+                        return hours + " hrs ago";
+                    }
+                } //Days
+                else {
+                    int days = Math.round(time_elapsed / 86400);
+                    if (days <= 7) {
+                        if (days == 1) {
+                            return "Yesterday";
+                        } else {
+                            return days + " days ago";
+                        }
+                    } //Weeks
+                    else {
+                        int weeks = Math.round(time_elapsed / 604800);
+                        if (weeks <= 4.3) {
+                            if (weeks == 1) {
+                                return "A week ago";
+                            } else {
+                                return weeks + " weeks ago";
+                            }
+                        } //Months
+                        else {
+                            int months = Math.round(time_elapsed / 2600640);
+                            if (months <= 12) {
+                                if (months == 1) {
+                                    return "A month ago";
+                                } else {
+                                    return months + " months ago";
+                                }
+                            } //Years
+                            else {
+                                int years = Math.round(time_elapsed / 31207680);
+                                if (years == 1) {
+                                    return "One year ago";
+                                } else {
+                                    return years + " years ago";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
     
 }
