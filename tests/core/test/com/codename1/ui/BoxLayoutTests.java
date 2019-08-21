codenameone/CodenameOne

commit 5c3174813a149da9865323285d877408c355bf29

Changed SpanLabel and SpanButton so that text is vertically aligned with the middle of the icon.  This is more consistent with the way that Label and Button are rendered.

CodenameOne/src/com/codename1/components/SpanLabel.java
@@ -75,7 +75,7 @@ public class SpanLabel extends Container {
         icon = new Label();
         icon.setUIID("icon");
         addComponent(BorderLayout.WEST, icon);
-        addComponent(BorderLayout.CENTER, text);
+        addComponent(BorderLayout.CENTER, FlowLayout.encloseLeftMiddle(text));
     }
     
     /**
@@ -204,7 +204,7 @@ public class SpanLabel extends Container {
                 if(text.getParent() != this) {
                     removeComponent(text.getParent());
                     text.getParent().removeAll();
-                    addComponent(BorderLayout.CENTER, text);
+                    addComponent(BorderLayout.CENTER, FlowLayout.encloseLeftMiddle(text));
                 }
         }
     }
@@ -225,7 +225,7 @@ public class SpanLabel extends Container {
         Container parent = text.getParent();
         if(parent == this) {
             parent.removeComponent(text);
-            parent = new Container(new FlowLayout(alignment));
+            parent = new Container(new FlowLayout(alignment, CENTER));
             parent.addComponent(text);
             addComponent(BorderLayout.CENTER, parent);
         } else {

CodenameOne/src/com/codename1/components/SpanButton.java
@@ -29,6 +29,7 @@ import com.codename1.ui.Image;
 import com.codename1.ui.TextArea;
 import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.FlowLayout;
 import com.codename1.ui.plaf.Style;
 
 /**
@@ -84,7 +85,7 @@ public class SpanButton extends Container {
         actualButton = new Button();
         actualButton.setUIID("icon");
         addComponent(BorderLayout.WEST, actualButton);
-        addComponent(BorderLayout.CENTER, text);
+        addComponent(BorderLayout.CENTER, FlowLayout.encloseLeftMiddle(text));
         setLeadComponent(actualButton);
     }
 

Samples/samples/SpanLabelIconAlignmentTest/SpanLabelIconAlignmentTest.java
@@ -1,0 +1,92 @@
+package com.codename1.samples;
+
+
+import com.codename1.components.SpanButton;
+import com.codename1.components.SpanLabel;
+import static com.codename1.ui.CN.*;
+import com.codename1.ui.Display;
+import com.codename1.ui.Form;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Label;
+import com.codename1.ui.plaf.UIManager;
+import com.codename1.ui.util.Resources;
+import com.codename1.io.Log;
+import com.codename1.ui.Toolbar;
+import java.io.IOException;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.io.NetworkEvent;
+import com.codename1.ui.FontImage;
+
+/**
+ * This file was generated by <a href="https://www.codenameone.com/">Codename One</a> for the purpose 
+ * of building native mobile applications using Java.
+ */
+public class SpanLabelIconAlignmentTest {
+
+    private Form current;
+    private Resources theme;
+    private static final String[] text = new String[]{
+        "Connect with your clients",
+        "View their activities and Saved searches",
+        "One Dashboard for important updates on properties",
+        "Receive feedback from clients on their favourite properties"
+    };
+
+    public void init(Object context) {
+        // use two network threads instead of one
+        updateNetworkThreadCount(2);
+
+        theme = UIManager.initFirstTheme("/theme");
+
+        // Enable Toolbar on all Forms by default
+        Toolbar.setGlobalToolbar(true);
+
+        // Pro only feature
+        Log.bindCrashProtection(true);
+
+        addNetworkErrorListener(err -> {
+            // prevent the event from propagating
+            err.consume();
+            if(err.getError() != null) {
+                Log.e(err.getError());
+            }
+            Log.sendLogAsync();
+            Dialog.show("Connection Error", "There was a networking error in the connection to " + err.getConnectionRequest().getUrl(), "OK", null);
+        });        
+    }
+    
+    public void start() {
+        if(current != null){
+            current.show();
+            return;
+        }
+        
+        Form hi = new Form("Hi World", BoxLayout.y());
+        FontImage icon = FontImage.createMaterial(FontImage.MATERIAL_FIBER_MANUAL_RECORD, new Label().getStyle(), 30);
+
+        for (String str : text) {
+            SpanLabel l = new SpanLabel(str);
+            l.setIcon(icon);
+            hi.add(l);
+        }
+        for (String str : text) {
+            SpanButton l = new SpanButton(str);
+            l.setIcon(icon);
+            hi.add(l);
+        }
+        hi.add(new Label("Hi World"));
+        hi.show();
+    }
+
+    public void stop() {
+        current = getCurrentForm();
+        if(current instanceof Dialog) {
+            ((Dialog)current).dispose();
+            current = getCurrentForm();
+        }
+    }
+    
+    public void destroy() {
+    }
+
+}

commit 78e0d4dadb04d1acff58efd9c1ac2005ba028700

Fixed regression in SpanLabel that caused applying alignment to the text style to have no effect.  This was caused by https://github.com/codenameone/CodenameOne/commit/5c3174813a149da9865323285d877408c355bf29 which aligned SpanLabel text vertically centered with the icon by default.  The problem was that this was achieved using a FlowLayout with vertical center alignment, but FlowLayout doesn't fill the bounds of the TextArea to fit its parent, so alignment within the TextArea would have no meaning anymore.  Unfortunately there was no layout that would achieve the effect that I wanted (vertically centered, but filling the child bounds horizontally), so I added support for this in BoxLayout. BoxLayout now includes an align property that will cause children to be laid out with a particular vertical alignment.  Y_AXIS supports CENTER and BOTTOM values for this. X_AXIS supports CENTER and RIGHT values for this.  This alignment has no effect if the children fill the the container.  It is only for aligning the empty space.  With this change, I was able to use a BoxLayout.Y with center alignment to wrap the TextArea and achieve the effect that I desired. Added a sample to test the regression which was provided in https://github.com/codenameone/CodenameOne/issues/2891. Also added unit tests for BoxLayout which can serve as a sample and prevent future regressions.

CodenameOne/src/com/codename1/components/SpanLabel.java
@@ -27,6 +27,7 @@ import com.codename1.ui.Image;
 import com.codename1.ui.Label;
 import com.codename1.ui.TextArea;
 import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.layouts.FlowLayout;
 import com.codename1.ui.plaf.Style;
 
@@ -80,7 +81,7 @@ public class SpanLabel extends Container {
         iconWrapper.getStyle().setPadding(0, 0, 0, 0);
         iconWrapper.add(icon);
         addComponent(BorderLayout.WEST, iconWrapper);
-        addComponent(BorderLayout.CENTER, FlowLayout.encloseLeftMiddle(text));
+        addComponent(BorderLayout.CENTER, BoxLayout.encloseYCenter(text));
     }
     
     /**
@@ -227,7 +228,7 @@ public class SpanLabel extends Container {
                 if(text.getParent() != this) {
                     removeComponent(text.getParent());
                     text.getParent().removeAll();
-                    addComponent(BorderLayout.CENTER, FlowLayout.encloseLeftMiddle(text));
+                    addComponent(BorderLayout.CENTER, BoxLayout.encloseYCenter(text));
                 }
         }
     }
@@ -247,6 +248,12 @@ public class SpanLabel extends Container {
     private void wrapText(int alignment) {
         Container parent = text.getParent();
         if(parent == this) {
+            parent.removeComponent(text);
+            parent = new Container(new FlowLayout(alignment, CENTER));
+            parent.addComponent(text);
+            addComponent(BorderLayout.CENTER, parent);
+        } else if (parent.getLayout() instanceof BoxLayout) {
+            removeComponent(parent);
             parent.removeComponent(text);
             parent = new Container(new FlowLayout(alignment, CENTER));
             parent.addComponent(text);

CodenameOne/src/com/codename1/ui/layouts/BoxLayout.java
@@ -89,6 +89,10 @@ public class BoxLayout extends Layout{
      */
     public static final int Y_AXIS_BOTTOM_LAST = 4;
     
+    /**
+     * @since 7.0
+     */
+    private int align=Component.TOP;
     
     private int axis;
     
@@ -119,11 +123,75 @@ public class BoxLayout extends Layout{
     }
     
     /**
+     * Creates a new layout with {@link #Y_AXIS}, and align center.
+     * @return BoxLayout with center alignment on Y_AXIS.
+     * @since 7.0
+     */
+    public static BoxLayout yCenter() {
+        BoxLayout out = new BoxLayout(BoxLayout.Y_AXIS);
+        out.setAlign(Component.CENTER);
+        return out;
+    }
+    
+    /**
+     * Creates a new layout with {@link #Y_AXIS}, and align bottom.
+     * @return BoxLayout with bottom alignment on Y_AXIS.
+     * @since 7.0
+     */
+    public static BoxLayout yBottom() {
+        BoxLayout out = new BoxLayout(BoxLayout.Y_AXIS);
+        out.setAlign(Component.BOTTOM);
+        return out;
+    }
+    
+    /**
      * Shorthand for {@code new BoxLayout(BoxLayout.X_AXIS)}
      * @return a new X axis {@code BoxLayout}
      */
     public static BoxLayout x() {
         return new BoxLayout(BoxLayout.X_AXIS);
+    }
+    
+    /**
+     * Creates a new layout with {@link #X_AXIS}, and align center.
+     * @return BoxLayout with center alignment on X_AXIS.
+     * @since 7.0
+     */
+    public static BoxLayout xCenter() {
+        BoxLayout out = new BoxLayout(BoxLayout.X_AXIS);
+        out.setAlign(Component.CENTER);
+        return out;
+    }
+    
+    /**
+     * Creates a new layout with {@link #X_AXIS}, and align right.
+     * @return BoxLayout with right alignment on X_AXIS.
+     * @since 7.0
+     */
+    public static BoxLayout xRight() {
+        BoxLayout out = new BoxLayout(BoxLayout.X_AXIS);
+        out.setAlign(Component.RIGHT);
+        return out;
+    }
+    
+    /**
+     * Sets the alignment of this layout. By default Y_AXIS aligns top, and X_AXIS aligns left (RTL-aware).  You can specify an align value of {@link Component#CENTER} to align items vertically centered (for Y_AXIS), and horizontally centered (for X_AXIS),
+     * of {@link Component#BOTTOM} to align vertically bottom (Y_AXIS), and {@link Component#RIGHT} to align right (RTL-aware), for X_AXIS.
+     * @param align One of {@link Component#CENTER}, {@link Component#BOTTOM}, {@link Component#RIGHT}, to adjust the alignment of children.
+     * @since 7.0
+     */
+    public void setAlign(int align) {
+        this.align = align;
+    }
+    
+    /**
+     * Gets the alignment of this layout.  By default Y_AXIS aligns top, and X_AXIS aligns left (RTL-aware).  You can specify an align value of {@link Component#CENTER} to align items vertically centered (for Y_AXIS), and horizontally centered (for X_AXIS),
+     * of {@link Component#BOTTOM} to align vertically bottom (Y_AXIS), and {@link Component#RIGHT} to align right (RTL-aware), for X_AXIS.
+     * @return The alignment.
+     * @since 7.0
+     */
+    public int getAlign() {
+        return this.align;
     }
 
     /**
@@ -227,7 +295,80 @@ public class BoxLayout extends Layout{
             if(cmp.getY() + cmp.getHeight() < height) {
                 cmp.setY(height - cmp.getHeight());
             }
-        }        
+        }
+        if (axis == Y_AXIS) {
+            
+            if (numOfcomponents > 0) {
+                int containerBottomInner = parent.getLayoutHeight() - parent.getStyle().getPaddingBottom();
+                Component lastCmp = parent.getComponentAt(numOfcomponents-1);
+                int lastCmpBottomOuter = lastCmp.getY() + lastCmp.getHeight() + lastCmp.getStyle().getMarginBottom();
+                int dy = 0;
+                switch (align) {
+                    case Component.CENTER: {
+                        dy = (containerBottomInner - lastCmpBottomOuter) / 2;
+                        break;
+                    }
+                    case Component.BOTTOM: {
+                        dy = (containerBottomInner - lastCmpBottomOuter);
+                        break;
+                    }
+                }
+                if (dy > 0) {
+                    for (int i=0; i<numOfcomponents; i++) {
+                        Component cmp = parent.getComponentAt(i);
+                        cmp.setY(cmp.getY() + dy);
+                    }
+                }
+            }
+            
+        } else if (axis == X_AXIS) {
+            if (numOfcomponents > 0) {
+                if (rtl) {
+                    int containerLeftInner = parent.getStyle().getPaddingLeftNoRTL();
+                    Component lastCmp = parent.getComponentAt(numOfcomponents-1);
+                    int lastCmpLeftOuter = lastCmp.getX() - lastCmp.getStyle().getMarginLeftNoRTL();
+                    int dx = 0;
+                    switch (align) {
+                        case Component.CENTER: {
+                            dx = (lastCmpLeftOuter - containerLeftInner) / 2;
+                            break;
+                        }
+                        case Component.RIGHT: {
+                            dx = (lastCmpLeftOuter - containerLeftInner);
+                            break;
+                        }
+                    }
+                    if (dx > 0) {
+                        for (int i=0; i<numOfcomponents; i++) {
+                            Component cmp = parent.getComponentAt(i);
+                            cmp.setX(cmp.getX() - dx);
+                        }
+                    }
+                } else {
+                    int containerRightInner = parent.getLayoutWidth() - parent.getStyle().getPaddingRightNoRTL();
+                    Component lastCmp = parent.getComponentAt(numOfcomponents-1);
+                    int lastCmpRightOuter = lastCmp.getX() + lastCmp.getWidth() + lastCmp.getStyle().getMarginRightNoRTL();
+                    int dx = 0;
+                    switch (align) {
+                        case Component.CENTER: {
+                            dx = (containerRightInner - lastCmpRightOuter) / 2;
+                            break;
+                        }
+                        case Component.RIGHT: {
+                            dx = (containerRightInner - lastCmpRightOuter);
+                            break;
+                        }
+                    }
+                    if (dx > 0) {
+                        for (int i=0; i<numOfcomponents; i++) {
+                            Component cmp = parent.getComponentAt(i);
+                            cmp.setX(cmp.getX() + dx);
+                        }
+                    }
+                }
+                
+            }
+        }
     }
     
     private Dimension dim = new Dimension(0, 0);
@@ -297,6 +438,26 @@ public class BoxLayout extends Layout{
     }
     
     /**
+     * The equivalent of Container.enclose() with a box layout Y, with center alignment.
+     * @param cmps the set of components
+     * @return the newly created container
+     * @since 7.0
+     */
+    public static Container encloseYCenter(Component... cmps) {
+        return Container.encloseIn(yCenter(), cmps);
+    }
+    
+    /**
+     * The equivalent of Container.enclose() with a box layout Y, with bottom alignment.
+     * @param cmps the set of components
+     * @return the newly created container
+     * @since 7.0
+     */
+    public static Container encloseYBottom(Component... cmps) {
+        return Container.encloseIn(yBottom(), cmps);
+    }
+    
+    /**
      * The equivalent of Container.enclose() with a box layout Y in bottom 
      * last mode
      * @param cmps the set of components
@@ -324,5 +485,25 @@ public class BoxLayout extends Layout{
      */
     public static Container encloseXNoGrow(Component... cmps) {
         return Container.encloseIn(new BoxLayout(BoxLayout.X_AXIS_NO_GROW), cmps);
+    }
+    
+    /**
+     * The equivalent of Container.enclose() with a box layout X, with center alignment.
+     * @param cmps the set of components
+     * @return the newly created container
+     * @since 7.0
+     */
+    public static Container encloseXCenter(Component... cmps) {
+        return Container.encloseIn(xCenter(), cmps);
+    }
+    
+    /**
+     * The equivalent of Container.enclose() with a box layout X, with right alignment.
+     * @param cmps the set of components
+     * @return the newly created container
+     * @since 7.0
+     */
+    public static Container encloseXRight(Component... cmps) {
+        return Container.encloseIn(xRight(), cmps);
     }
 }

Samples/samples/SpanLabelTestAllStyles2891/SpanLabelTestAllStyles2891.java
@@ -1,0 +1,71 @@
+package com.codename1.samples;
+
+
+import com.codename1.components.SpanLabel;
+import static com.codename1.ui.CN.*;
+import com.codename1.ui.Form;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.plaf.UIManager;
+import com.codename1.ui.util.Resources;
+import com.codename1.io.Log;
+import com.codename1.ui.Toolbar;
+import com.codename1.ui.Component;
+import com.codename1.ui.layouts.LayeredLayout;
+
+/**
+ * This file was generated by <a href="https://www.codenameone.com/">Codename One</a> for the purpose 
+ * of building native mobile applications using Java.
+ */
+public class SpanLabelTestAllStyles2891 {
+
+    private Form current;
+    private Resources theme;
+
+    public void init(Object context) {
+        // use two network threads instead of one
+        updateNetworkThreadCount(2);
+
+        theme = UIManager.initFirstTheme("/theme");
+
+        // Enable Toolbar on all Forms by default
+        Toolbar.setGlobalToolbar(true);
+
+        // Pro only feature
+        Log.bindCrashProtection(true);
+
+        addNetworkErrorListener(err -> {
+            // prevent the event from propagating
+            err.consume();
+            if(err.getError() != null) {
+                Log.e(err.getError());
+            }
+            Log.sendLogAsync();
+            Dialog.show("Connection Error", "There was a networking error in the connection to " + err.getConnectionRequest().getUrl(), "OK", null);
+        });        
+    }
+    
+    public void start() {
+        if(current != null){
+            current.show();
+            return;
+        }
+        Form f = new Form("Spanny", new LayeredLayout());
+        SpanLabel body = new SpanLabel("Just a text!");
+        body.getTextAllStyles().setAlignment(Component.CENTER);
+        f.add(body);
+        
+        f.show();
+    }
+
+    public void stop() {
+        current = getCurrentForm();
+        if(current instanceof Dialog) {
+            ((Dialog)current).dispose();
+            current = getCurrentForm();
+        }
+    }
+    
+    public void destroy() {
+    }
+
+}

tests/core/test/com/codename1/ui/BoxLayoutTests.java
@@ -1,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codename1.ui;
+
+import com.codename1.testing.AbstractTest;
+import static com.codename1.ui.CN.BOTTOM;
+import static com.codename1.ui.CN.CENTER;
+import static com.codename1.ui.CN.RIGHT;
+import static com.codename1.ui.ComponentSelector.$;
+import com.codename1.ui.geom.Dimension;
+import com.codename1.ui.layouts.BoxLayout;
+
+/**
+ *
+ * @author shannah
+ */
+public class BoxLayoutTests extends AbstractTest {
+
+    @Override
+    public boolean runTest() throws Exception {
+        BoxLayout l = new BoxLayout(BoxLayout.Y_AXIS);
+        Container cnt = new Container(l);
+        cnt.setRTL(false);
+        int w = 500;
+        int h = 500;
+
+        cnt.setWidth(w);
+        cnt.setHeight(h);
+
+        Component child1 = createEmptyComponent(100, 100);
+        Component child2 = createEmptyComponent(200, 50);
+        cnt.add(child1).add(child2);
+
+        $(child1, child2, cnt).setPadding(0).setMargin(0);
+        cnt.layoutContainer();
+        assertEqual(0, child1.getY(), "child1 should be aligned top");
+        assertEqual(100, child2.getY(), "child 2 should be aligned top just after child1 ");
+        assertEqual(0, child1.getX(), "Child1 not aligned left");
+        assertEqual(w, child1.getWidth(), "Child1 not taking full width");
+        assertEqual(0, child2.getX(), "Child2 not aligned left");
+        assertEqual(w, child2.getWidth(), "Child2 not taking full width");
+
+        l.setAlign(BOTTOM);
+        cnt.setShouldCalcPreferredSize(true);
+        cnt.layoutContainer();
+        assertEqual(500, child2.getY() + child2.getHeight(), "Child2 should be aligned bottom");
+        assertEqual(450, child1.getY() + child1.getHeight(), "child 1 should be aligned bottom just above child2");
+        assertEqual(0, child1.getX(), "Child1 not aligned left");
+        assertEqual(w, child1.getWidth(), "Child1 not taking full width");
+        assertEqual(0, child2.getX(), "Child2 not aligned left");
+        assertEqual(w, child2.getWidth(), "Child2 not taking full width");
+
+        l.setAlign(CENTER);
+        cnt.setShouldCalcPreferredSize(true);
+        cnt.layoutContainer();
+        assertEqual(325, child2.getY() + child2.getHeight(), "Child2 should be aligned bottom");
+        assertEqual(175, child1.getY(), "child 1 should be aligned bottom just above child2");
+        assertEqual(0, child1.getX(), "Child1 not aligned left");
+        assertEqual(w, child1.getWidth(), "Child1 not taking full width");
+        assertEqual(0, child2.getX(), "Child2 not aligned left");
+        assertEqual(w, child2.getWidth(), "Child2 not taking full width");
+
+        Component child3 = createEmptyComponent(500, 500);
+        $(child3).setPadding(0).setMargin(0);
+        cnt.add(child3);
+        // This is a component to tip it over the edge.
+
+        // NOTICE:  When the children fill the height of the container, the align property
+        // ceases to have meaning.  We do NOT try to align the components once the container is filled.
+        cnt.setShouldCalcPreferredSize(true);
+        cnt.layoutContainer();
+        assertEqual(0, child1.getY(), "Child1 should be aligned top");
+        assertEqual(100, child2.getY(), "Child 2 should be aligned just after");
+        assertEqual(150, child3.getY(), "Child 3 should be just after");
+
+        assertEqual(0, child1.getX(), "Child1 not aligned left");
+        assertEqual(w, child1.getWidth(), "Child1 not taking full width");
+        assertEqual(0, child2.getX(), "Child2 not aligned left");
+        assertEqual(w, child2.getWidth(), "Child2 not taking full width");
+
+        l.setAlign(BOTTOM);
+        cnt.setShouldCalcPreferredSize(true);
+        cnt.layoutContainer();
+        assertEqual(0, child1.getY(), "Child1 should be aligned top");
+        assertEqual(100, child2.getY(), "Child 2 should be aligned just after");
+        assertEqual(150, child3.getY(), "Child 3 should be just after");
+
+        assertEqual(0, child1.getX(), "Child1 not aligned left");
+        assertEqual(w, child1.getWidth(), "Child1 not taking full width");
+        assertEqual(0, child2.getX(), "Child2 not aligned left");
+        assertEqual(w, child2.getWidth(), "Child2 not taking full width");
+
+        //Now test the x axis
+        l = BoxLayout.x();
+        cnt.setLayout(l);
+        cnt.removeComponent(child3);
+        cnt.setShouldCalcPreferredSize(true);
+        cnt.layoutContainer();
+
+        assertEqual(0, child1.getX(), "Child1 not aligned left");
+        assertEqual(100, child1.getWidth(), "Child2 not taking preferred width");
+        assertEqual(h, child1.getHeight(), "Child1 not taking full height");
+        assertEqual(0, child1.getY(), "Child1 not aligning top");
+        assertEqual(100, child2.getX(), "Child2 should be aligned next to child1");
+        assertEqual(200, child2.getWidth(), "Child 2 not taking preferred width");
+        assertEqual(h, child2.getHeight(), "Child 2 not taking full height of container");
+        assertEqual(0, child2.getY(), "Child2 not aligning top");
+
+        l.setAlign(CENTER);
+        cnt.setShouldCalcPreferredSize(true);
+        cnt.layoutContainer();
+
+        assertEqual(100, child1.getX(), "Child1 not aligned left");
+        assertEqual(100, child1.getWidth(), "Child2 not taking preferred width");
+        assertEqual(h, child1.getHeight(), "Child1 not taking full height");
+        assertEqual(0, child1.getY(), "Child1 not aligning top");
+        assertEqual(200, child2.getX(), "Child2 should be aligned next to child1");
+        assertEqual(200, child2.getWidth(), "Child 2 not taking preferred width");
+        assertEqual(h, child2.getHeight(), "Child 2 not taking full height of container");
+        assertEqual(0, child2.getY(), "Child2 not aligning top");
+
+        l.setAlign(RIGHT);
+        cnt.setShouldCalcPreferredSize(true);
+        cnt.layoutContainer();
+
+        assertEqual(200, child1.getX(), "Child1 not aligned left");
+        assertEqual(100, child1.getWidth(), "Child2 not taking preferred width");
+        assertEqual(h, child1.getHeight(), "Child1 not taking full height");
+        assertEqual(0, child1.getY(), "Child1 not aligning top");
+        assertEqual(300, child2.getX(), "Child2 should be aligned next to child1");
+        assertEqual(200, child2.getWidth(), "Child 2 not taking preferred width");
+        assertEqual(h, child2.getHeight(), "Child 2 not taking full height of container");
+        assertEqual(0, child2.getY(), "Child2 not aligning top");
+
+        return true;
+    }
+
+    private Component createEmptyComponent(int width, int height) {
+        return new Component() {
+            @Override
+            protected Dimension calcPreferredSize() {
+                return new Dimension(width, height);
+            }
+
+        };
+
+    }
+}