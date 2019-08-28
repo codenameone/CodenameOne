b2a7b0482da3ec464046868bbf7b5a1639cd8008
Shai Almog <shai@codenameone.com>
2019-08-28 12:38:25 +0300

    Fixes for javadoc

--- CodenameOne/build_javadoc.sh
+++ CodenameOne/build_javadoc.sh
@@ -4,4 +4,4 @@ rm -Rf dist/javadoc
 rm -Rf build/tempJavaSources
 java -jar ~/dev/java/JavaDocSourceEmbed/target/JavaDocSourceEmbed-1.0-SNAPSHOT.jar src build/tempJavaSources
 
-/usr/bin/find build/tempJavaSources ../Ports/CLDC11/src -name "*.java" | /usr/bin/grep -v /impl/ | /usr/bin/xargs javadoc -protected -d dist/javadoc -windowtitle "Codename One API"
+/usr/bin/find build/tempJavaSources ../Ports/CLDC11/src -name "*.java" | /usr/bin/grep -v /impl/ | /usr/bin/xargs javadoc --allow-script-in-comments -protected -d dist/javadoc -windowtitle "Codename One API"
