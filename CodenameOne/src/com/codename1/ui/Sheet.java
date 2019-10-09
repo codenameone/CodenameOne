3fc47e5fc2e65ea5de6eee6f2a2cb1fdaf0cea18
Shai Almog <shai@codenameone.com>
2019-10-09 06:44:44 +0300

    Fixed wayward event in sheet
    If you click outside the sheet the event is still dispatched to the underlying button https://groups.google.com/d/msg/codenameone-discussions/KaX42-aU8u8/PmvNCBzyAQAJ

--- CodenameOne/src/com/codename1/ui/Sheet.java
+++ CodenameOne/src/com/codename1/ui/Sheet.java
@@ -143,6 +143,7 @@ public class Sheet extends Container {
             if (Sheet.this.contains(cmp) || Sheet.this == cmp || cmp.isOwnedBy(Sheet.this)) {
                 // do nothing.
             } else {
+                evt.consume();
                 hide(DEFAULT_TRANSITION_DURATION);
             }
         }
