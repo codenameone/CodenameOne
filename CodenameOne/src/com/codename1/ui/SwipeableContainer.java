6e44878c1900895b81c05b3638efc7bb578bbd7b
Thomas Hjelm <thomas.hjelm@email.com>
2020-05-05 11:13:06 +0200

    Added support for closing an already open SwipeableContainer.

--- CodenameOne/src/com/codename1/ui/SwipeableContainer.java
+++ CodenameOne/src/com/codename1/ui/SwipeableContainer.java
@@ -383,6 +383,8 @@ public class SwipeableContainer extends Container {
                     }
 
                     if (initialX != -1) {
+                        if (getPreviouslyOpened() != null && getPreviouslyOpened() != SwipeableContainer.this && getPreviouslyOpened().isOpen())
+                            getPreviouslyOpened().close();
                         int diff = x - initialX;
                         int val = 0;
                         if(!isOpen()){
@@ -444,4 +446,12 @@ public class SwipeableContainer extends Container {
         }
     }
 
+    /**
+     * override to return a previously opened SwipeableContainer that should be automatically closed when starting to open this one
+     * @return 
+     */
+    public SwipeableContainer getPreviouslyOpened() {
+        return null;
+    }
+    
 }
