619bae28e9b0b922b6d5edc596162ff40d397bf8
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2020-08-12 06:19:34 +0300

    Another compilation issue in the last PR

--- CodenameOne/src/com/codename1/io/rest/RequestBuilder.java
+++ CodenameOne/src/com/codename1/io/rest/RequestBuilder.java
@@ -658,7 +658,7 @@ public class RequestBuilder {
      * @param root the root element's key of the structured content
      * @return returns the Connection Request object so it can be killed if necessary
      */
-    public ConnectionRequest fetchAsPropertyList(final OnComplete<Response<List<PropertyBusinessObject>>> callback, final Class type, String root) {
+    public ConnectionRequest fetchAsPropertyList(final OnComplete<Response<List<PropertyBusinessObject>>> callback, final Class type, final String root) {
         final Connection request = createRequest(true);
         request.addResponseListener(new ActionListener<NetworkEvent>() {
             @Override
