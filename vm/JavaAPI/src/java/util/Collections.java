6d1840899854969389a19d56924bc7dc0f37bd2e
Steve Hannah <steve@weblite.ca>
2020-10-21 07:34:10 -0700

    Fixed bug in ParparVM's Collections.min() method. It was actually returning max().

--- vm/JavaAPI/src/java/util/Collections.java
+++ vm/JavaAPI/src/java/util/Collections.java
@@ -1654,7 +1654,7 @@ public class Collections {
         Iterator<? extends T> it = collection.iterator();
         T min = it.next();
         if ( NumberComparator.isNumber(min)){
-            return (T)max(collection, NumberComparator.createComparator(min.getClass()));
+            return (T)min(collection, NumberComparator.createComparator(min.getClass()));
         }
         while (it.hasNext()) {
             T next = it.next();
