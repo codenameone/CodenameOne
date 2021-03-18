717254349e02a006500e7c7bc4534aca0abb106d
Steve Hannah <steve@weblite.ca>
2021-03-18 10:37:22 -0700

    Added googlemaps demo to integration tests.

--- maven/integration-tests/all.sh
+++ maven/integration-tests/all.sh
@@ -5,3 +5,4 @@ source $SCRIPTPATH/inc/env.sh
 bash cn1app-archetype-test.sh
 bash bare-bones-kotlin-test.sh
 bash  migrate-kitchensink-test.sh
+bash googlemaps-demo.sh
---
+++ maven/integration-tests/googlemaps-demo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+set -e
+source $SCRIPTPATH/inc/env.sh
+cd $SCRIPTPATH/build
+if [ -d googlemapsdemo ]; then
+  rm -rf googlemapsdemo
+fi
+
+curl -L https://github.com/shannah/googlemaps-maven-demo/archive/v1.0.2.zip > master.zip
+unzip master.zip
+rm master.zip
+mvn com.codenameone:codenameone-maven-plugin:${CN1_VERSION}:generate-app-project \
+  -DarchetypeGroupId=com.codename1 \
+  -DarchetypeArtifactId=cn1app-archetype \
+  -DarchetypeVersion=${CN1_VERSION} \
+  -DartifactId=googlemapsdemo \
+  -DgroupId=com.example \
+  -Dversion=1.0-SNAPSHOT \
+  -DinteractiveMode=false \
+  -DsourceProject=googlemaps-maven-demo-1.0.2
+
+rm -rf googlemaps-maven-demo-1.0.2
+
+cd googlemapsdemo
+chmod 755 build.sh
+./build.sh jar
+if [ -d /Applications/Xcode.app ]; then
+  "mvn" "package" "-DskipTests" "-Dcodename1.platform=ios" "-Dcodename1.buildTarget=ios-source" -Dopen=false
+fi
+if [ -d $HOME/Library/Android/sdk ]; then
+  "mvn" "package" "-DskipTests" "-Dcodename1.platform=android" "-Dcodename1.buildTarget=android-source" -Dopen=false
+fi
+
+
