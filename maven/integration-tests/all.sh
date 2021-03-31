4af13ecf17f78f652bd44c9147b83832b7200e97
Steve Hannah <steve@weblite.ca>
2021-03-31 13:56:58 -0700

    Added googlemaps demo integration test to set of all tests that are run by github actions.

--- maven/integration-tests/all.sh
+++ maven/integration-tests/all.sh
@@ -2,6 +2,7 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 set -e
 source $SCRIPTPATH/inc/env.sh
+bash migrate-googlemapsdemo-test.sh
 bash migrate-coderad-lib.sh
 bash cssfonts.sh
 bash native-interfaces.sh
