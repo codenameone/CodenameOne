b6d9db11a8b33614e7478b92c8507db968a36282
Shai Almog <shai.almog@codenameone.com>
2021-06-25 06:19:10 +0300

    Removed warning

--- Ports/JavaSE/src/com/codename1/impl/javase/JavaSEPort.java
+++ Ports/JavaSE/src/com/codename1/impl/javase/JavaSEPort.java
@@ -713,7 +713,6 @@ public class JavaSEPort extends CodenameOneImplementation {
     static LocationSimulation locSimulation;
     static PushSimulator pushSimulation;
     private static boolean blockMonitors;
-    protected static boolean fxExists = false;
     private JFrame window;
     private long lastIdleTime;
     private static boolean showEDTWarnings = true;
@@ -3320,15 +3319,10 @@ public class JavaSEPort extends CodenameOneImplementation {
 
                 @Override
                 public void actionPerformed(ActionEvent ae) {
-                    if(!fxExists){
-                        System.err.println("This simulation requires a JDK with JavaFX");
-                        return;
-                    }
                     if(locSimulation==null) {
                             locSimulation = new LocationSimulation();
                     }
-                            locSimulation.setVisible(true);
-                    
+                    locSimulation.setVisible(true);
                 }
             });
             simulateMenu.add(locactionSim);
