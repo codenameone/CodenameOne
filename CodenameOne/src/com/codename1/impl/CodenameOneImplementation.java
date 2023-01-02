6e500abe1dfa2a45fac3efc3bb661c99fef5f7e3
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2023-01-02 19:40:15 +0200

    Oops

--- CodenameOne/src/com/codename1/impl/CodenameOneImplementation.java
+++ CodenameOne/src/com/codename1/impl/CodenameOneImplementation.java
@@ -979,7 +979,6 @@ public abstract class CodenameOneImplementation {
         
         for(int y = 0 ; y < height ; y++) {
             for(int x = 0 ; x < width ; x++) {
-                newRGB[y + x * height] = rgb[x + y * width];
                 newRGB[y + (width - x - 1) * height] = rgb[x + y * width];
             }
         }
