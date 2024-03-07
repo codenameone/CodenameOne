codenameone/CodenameOne

commit cb2244b281838b15b23ebbaa483e0d63471180a5

Update CodenameOneView.java (#3785)
* Update CodenameOneView.java

user DB107 made wrong changes, this is correct commit

* Update CodenameOneView.java

Ports/Android/src/com/codename1/impl/android/CodenameOneView.java
@@ -277,9 +277,12 @@ public class CodenameOneView {
             case KeyEvent.KEYCODE_SYM:
             return false;
             case KeyEvent.KEYCODE_ENTER:
-                Display.getInstance().keyPressed(keyCode);
-                Display.getInstance().keyReleased(keyCode);
-                break;
+            	if (down) {
+                    Display.getInstance().keyPressed(keyCode);
+                } else {
+                    Display.getInstance().keyReleased(keyCode);
+                }
+            	return false;
             default:
         }
 

commit 4bdbf3d1b6ed3dddee5f8d2b10a1a95a89ea8fa7

Fixed regression in text editing
This was caused by https://github.com/codenameone/CodenameOne/commit/cb2244b281838b15b23ebbaa483e0d63471180a5

Ports/Android/src/com/codename1/impl/android/CodenameOneView.java
@@ -277,12 +277,16 @@ public class CodenameOneView {
             case KeyEvent.KEYCODE_SYM:
             return false;
             case KeyEvent.KEYCODE_ENTER:
-            	if (down) {
-                    Display.getInstance().keyPressed(keyCode);
-                } else {
-                    Display.getInstance().keyReleased(keyCode);
+                if(Display.getInstance().getProperty("sendEnterKey", "false").equals("true")) {
+                	if (down) {
+                        Display.getInstance().keyPressed(keyCode);
+                    } else {
+                        Display.getInstance().keyReleased(keyCode);
+                    }
+               	    return false;
                 }
-            	return false;
+                break;
+                
             default:
         }
 