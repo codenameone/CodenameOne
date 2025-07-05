codenameone/CodenameOne

commit a87ef18386563d6726035da904f605936c9d8d75

fix: [3904] Fixed android async editing with form resizing on API 34+ (#3905)
* fix: [3904] Fixed android async editing with form resizing on API 34+

Android 34+ no longer supports the WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE parameter for resizing the form for the soft editor.
This broke our in place editor when using formbottompadding editing mode as described in https://github.com/codenameone/CodenameOne/issues/3904.

This fix uses a different approach to add the insets manually.

* replaced lambda, because not supported on source 1.6

* fixed build error

Ports/Android/src/com/codename1/impl/android/InPlaceEditView.java
@@ -58,6 +58,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
@@ -1376,12 +1378,56 @@ public class InPlaceEditView extends FrameLayout{
 
 
 
-    private static void setEditMode(final boolean resize){
+    private static void setEditMode(final boolean resize) {
         resizeMode = resize;
+
+        final Activity activity = sInstance.impl.getActivity();
+        final Window window = activity.getWindow();
+
         if (resize) {
-            sInstance.impl.getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+            if (Build.VERSION.SDK_INT >= 34) {
+                // On Android 14+, adjustResize doesn't work with immersive layouts
+                window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+                View rootView = window.getDecorView().findViewById(android.R.id.content);
+                applyImeInsetPaddingReflection(rootView);
+            } else {
+                // Old behavior works fine
+                window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+            }
         } else {
-            sInstance.impl.getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        }
+    }
+
+
+    private static void applyImeInsetPaddingReflection(View rootView) {
+        try {
+            rootView.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+                @Override
+                public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+                    try {
+                        Class<?> typeClass = Class.forName("android.view.WindowInsets$Type");
+                        Class<?> insetsClass = Class.forName("android.view.WindowInsets");
+                        int imeType = ((Integer) typeClass.getMethod("ime").invoke(null)).intValue();
+
+                        Object imeInsets = insetsClass.getMethod("getInsets", int.class).invoke(insets, imeType);
+                        Class<?> insetsRectClass = imeInsets.getClass();
+
+                        int bottom = ((Integer) insetsRectClass.getField("bottom").get(imeInsets)).intValue();
+                        boolean isVisible = ((Boolean) insetsClass.getMethod("isVisible", int.class).invoke(insets, imeType)).booleanValue();
+
+                        v.setPadding(0, 0, 0, isVisible ? bottom : 0);
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
+                    return insets;
+                }
+            });
+
+            rootView.requestApplyInsets();
+
+        } catch (Throwable e) {
+            e.printStackTrace();
         }
     }
 

commit 4f55db78ec1a0f7f9df7b45c9eee55c36605a7f4

fix: [3909] Fixed regression in android async editing
Fixes https://github.com/codenameone/CodenameOne/issues/3909

Caused by https://github.com/codenameone/CodenameOne/pull/3905, which fixed
https://github.com/codenameone/CodenameOne/issues/3904

Ports/Android/src/com/codename1/impl/android/InPlaceEditView.java
@@ -1376,8 +1376,6 @@ public class InPlaceEditView extends FrameLayout{
         }
     }
 
-
-
     private static void setEditMode(final boolean resize) {
         resizeMode = resize;
 
@@ -1385,7 +1383,7 @@ public class InPlaceEditView extends FrameLayout{
         final Window window = activity.getWindow();
 
         if (resize) {
-            if (Build.VERSION.SDK_INT >= 34) {
+            if (Build.VERSION.SDK_INT >= 34 && isImmersive(window)) {
                 // On Android 14+, adjustResize doesn't work with immersive layouts
                 window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
                 View rootView = window.getDecorView().findViewById(android.R.id.content);
@@ -1399,6 +1397,53 @@ public class InPlaceEditView extends FrameLayout{
         }
     }
 
+    private static boolean isImmersive(Window window) {
+        View decor = window.getDecorView();
+
+        // ---------- Modern branch : API 30+ ----------
+        if (Build.VERSION.SDK_INT >= 30) {
+            try {
+                // WindowInsets insets = decor.getRootWindowInsets();
+                Object insets = View.class
+                        .getMethod("getRootWindowInsets")
+                        .invoke(decor);
+                if (insets == null) return false;
+
+                // int mask = WindowInsets.Type.systemBars();
+                Class<?> typeCls =
+                        Class.forName("android.view.WindowInsets$Type");
+                int systemBars = (Integer) typeCls
+                        .getMethod("systemBars")
+                        .invoke(null);
+
+                // boolean barsVisible = insets.isVisible(mask);
+                boolean barsVisible = (Boolean) insets.getClass()
+                        .getMethod("isVisible", int.class)
+                        .invoke(insets, systemBars);
+
+                /* --------------------------------------------------
+                 * Edge-to-edge is guaranteed on API 35+ anyway, and
+                 * `getDecorFitsSystemWindows()` no longer exists on
+                 * API 36.  We treat everything >=30 as edge-to-edge
+                 * (decorFits = false) and only look at bar visibility
+                 * to decide if we’re *immersive*.
+                 * -------------------------------------------------- */
+                return !barsVisible;    // immersive ⇢ bars are hidden
+            } catch (Throwable ignore) {
+                System.out.println("Error: " + ignore);
+            }
+        }
+
+        // ---------- Legacy branch : API 19-29 ----------
+        int f = decor.getSystemUiVisibility();
+        boolean immersiveBits =
+                (f & View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) != 0 ||
+                        (f & View.SYSTEM_UI_FLAG_IMMERSIVE)        != 0;
+        boolean barsHidden =
+                (f & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) != 0 &&
+                        (f & View.SYSTEM_UI_FLAG_FULLSCREEN)      != 0;
+        return immersiveBits && barsHidden;
+    }
 
     private static void applyImeInsetPaddingReflection(View rootView) {
         try {