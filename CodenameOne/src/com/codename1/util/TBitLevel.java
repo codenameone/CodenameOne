7714ecf2e61e651651c786339faf10a55078c990
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2025-10-18 16:34:51 +0300

    Restore J2ME-compatible leading zero calculation

--- CodenameOne/src/com/codename1/util/TBitLevel.java
+++ CodenameOne/src/com/codename1/util/TBitLevel.java
@@ -64,7 +64,29 @@ class TBitLevel {
     }
 
     static int numberOfLeadingZeros(int i) {
-        return Integer.numberOfLeadingZeros(i);
+        if (i == 0) {
+            return 32;
+        }
+
+        int n = 1;
+        if ((i >>> 16) == 0) {
+            n += 16;
+            i <<= 16;
+        }
+        if ((i >>> 24) == 0) {
+            n += 8;
+            i <<= 8;
+        }
+        if ((i >>> 28) == 0) {
+            n += 4;
+            i <<= 4;
+        }
+        if ((i >>> 30) == 0) {
+            n += 2;
+            i <<= 2;
+        }
+        n -= (i >>> 31);
+        return n;
     }
 
     /**
