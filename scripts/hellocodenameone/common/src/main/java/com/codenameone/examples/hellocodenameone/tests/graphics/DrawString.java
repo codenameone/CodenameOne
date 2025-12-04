ea3021963094fe2931c1aff50d7d08646f9e8ef8
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2025-12-04 04:59:48 +0200

    Initial refactor of the graphics pipeline

---
+++ scripts/hellocodenameone/common/src/main/java/com/codenameone/examples/hellocodenameone/tests/graphics/DrawString.java
@@ -0,0 +1,34 @@
+package com.codenameone.examples.hellocodenameone.tests.graphics;
+
+import com.codename1.ui.Font;
+import com.codename1.ui.Graphics;
+import com.codename1.ui.geom.Rectangle;
+import com.codenameone.examples.hellocodenameone.tests.AbstractGraphicsScreenshotTest;
+
+public class DrawString extends AbstractGraphicsScreenshotTest {
+    @Override
+    protected void drawContent(Graphics g, Rectangle bounds) {
+        g.setColor(0);
+        g.fillShape(bounds);
+        g.setColor(0xffffff);
+        int y = bounds.getY();
+        g.drawString("Default Font", bounds.getX(), y);
+        y += g.getFont().getHeight();
+        g.setFont(Font.createSystemFont(Font.FACE_MONOSPACE, Font.STYLE_BOLD, Font.SIZE_SMALL));
+        g.drawString("Small Bold Monospace", bounds.getX(), y);
+        y += g.getFont().getHeight();
+        String[] ttfFonts = {"native:MainThin", "native:MainLight", "native:MainRegular",
+                "native:MainBold", "native:MainBlack", "native:ItalicThin", "native:ItalicLight",
+                "native:ItalicRegular", "native:ItalicBold", "native:ItalicBlack"};
+        for(String name : ttfFonts) {
+            g.setFont(Font.createTrueTypeFont(name, 4));
+            g.drawString(name, bounds.getX(), y);
+            y += g.getFont().getHeight();
+        }
+    }
+
+    @Override
+    protected String screenshotName() {
+        return "graphics-draw-string";
+    }
+}
---
+++ scripts/hellocodenameone/common/src/main/java/com/codenameone/examples/hellocodenameone/tests/graphics/DrawStringDecorated.java
@@ -0,0 +1,35 @@
+package com.codenameone.examples.hellocodenameone.tests.graphics;
+
+import com.codename1.ui.Font;
+import com.codename1.ui.Graphics;
+import com.codename1.ui.geom.Rectangle;
+import com.codename1.ui.plaf.Style;
+import com.codenameone.examples.hellocodenameone.tests.AbstractGraphicsScreenshotTest;
+
+public class DrawStringDecorated extends AbstractGraphicsScreenshotTest {
+    @Override
+    protected void drawContent(Graphics g, Rectangle bounds) {
+        g.setColor(0xffffff);
+        g.fillShape(bounds);
+        g.setColor(0);
+        int y = bounds.getY();
+        g.drawString("No Decoration", bounds.getX(), y, Style.TEXT_DECORATION_NONE);
+        y += g.getFont().getHeight();
+        g.drawString("3D Decoration", bounds.getX(), y, Style.TEXT_DECORATION_3D);
+        y += g.getFont().getHeight();
+        g.drawString("3D Lowered Decoration", bounds.getX(), y, Style.TEXT_DECORATION_3D_LOWERED);
+        y += g.getFont().getHeight();
+        g.drawString("Overline Decoration", bounds.getX(), y, Style.TEXT_DECORATION_OVERLINE);
+        y += g.getFont().getHeight();
+        g.drawString("Strikethru Decoration", bounds.getX(), y, Style.TEXT_DECORATION_STRIKETHRU);
+        y += g.getFont().getHeight();
+        g.drawString("3D Shadow North Decoration", bounds.getX(), y, Style.TEXT_DECORATION_3D_SHADOW_NORTH);
+        y += g.getFont().getHeight();
+        g.drawString("Underline Decoration", bounds.getX(), y, Style.TEXT_DECORATION_UNDERLINE);
+    }
+
+    @Override
+    protected String screenshotName() {
+        return "graphics-draw-string-decorated";
+    }
+}
