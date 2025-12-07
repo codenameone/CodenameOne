f8d4f94fde344b4c947e658c589c75410371488d
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2025-12-07 06:59:05 +0200

    Fixed fill polygon test

--- scripts/hellocodenameone/common/src/main/java/com/codenameone/examples/hellocodenameone/tests/graphics/FillPolygon.java
+++ scripts/hellocodenameone/common/src/main/java/com/codenameone/examples/hellocodenameone/tests/graphics/FillPolygon.java
@@ -12,14 +12,21 @@ public class FillPolygon extends AbstractGraphicsScreenshotTest {
         int[] yPoints = new int[360];
         int centerX = bounds.getX() + bounds.getWidth() / 2;
         int centerY = bounds.getY() + bounds.getHeight() / 2;
-        int radius = bounds.getWidth() / 2;
+        int radius = bounds.getWidth() / 3;
         for(int i = 0; i < 360 ; i++) {
             double angle = 2 * Math.PI * i / 360.0;
             xPoints[i] = centerX + (int) Math.round(radius * Math.cos(angle));
             yPoints[i] = centerY + (int) Math.round(radius * Math.sin(angle));
         }
 
+        g.setColor(0xffffff);
+        g.fillRect(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight());
+
+        g.setColor(0);
         g.fillPolygon(xPoints, yPoints, 360);
+        
+        g.setColor(0xff);
+        g.drawPolygon(xPoints, yPoints, 360);
     }
 
     @Override
