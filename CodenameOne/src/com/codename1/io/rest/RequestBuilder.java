07bf1cf9218b6a294f756d6476b0d6bdf6c8876e
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2026-01-13 16:22:25 +0200

    Fix static helper access in RequestBuilder

--- CodenameOne/src/com/codename1/io/rest/RequestBuilder.java
+++ CodenameOne/src/com/codename1/io/rest/RequestBuilder.java
@@ -821,13 +821,13 @@ public class RequestBuilder {
         return req;
     }
 
-    private PropertyBusinessObject createBusinessObject(Class type, Map response) throws InstantiationException, IllegalAccessException {
+    private static PropertyBusinessObject createBusinessObject(Class type, Map response) throws InstantiationException, IllegalAccessException {
         PropertyBusinessObject pb = (PropertyBusinessObject) type.newInstance();
         pb.getPropertyIndex().populateFromMap(response);
         return pb;
     }
 
-    private List<PropertyBusinessObject> buildBusinessObjectList(Class type, List<Map> lst) throws InstantiationException, IllegalAccessException {
+    private static List<PropertyBusinessObject> buildBusinessObjectList(Class type, List<Map> lst) throws InstantiationException, IllegalAccessException {
         List<PropertyBusinessObject> result = new ArrayList<PropertyBusinessObject>();
         for (Map m : lst) {
             result.add(createBusinessObject(type, m));
