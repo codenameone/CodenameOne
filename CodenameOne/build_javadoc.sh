8957f6ef5c776c8938bd2f8b4b4448a923826270
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2026-02-02 20:26:44 +0200

    Move Javadoc build script into CI workflows (#4481)

--- CodenameOne/build_javadoc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -Rf dist/javadoc
-rm -Rf build/tempJavaSources
-java -jar ~/dev/java/JavaDocSourceEmbed/target/JavaDocSourceEmbed-1.0-SNAPSHOT.jar src build/tempJavaSources
-
-/usr/bin/find build/tempJavaSources ../Ports/CLDC11/src -name "*.java" | /usr/bin/grep -v /impl/ | /usr/bin/xargs javadoc --allow-script-in-comments -protected -d dist/javadoc -windowtitle "Codename One API"
