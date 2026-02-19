84ca532679065e483dfa29234795d45b05e68c03
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2026-02-19 03:50:57 +0200

    Another attempt to redirect to the developer guide properly

--- docs/website/functions/[[path]].js
+++ docs/website/functions/[[path]].js
@@ -2,16 +2,6 @@ export async function onRequest(context) {
   const url = new URL(context.request.url);
   const path = url.pathname;
 
-  // Keep /developer-guide.html as canonical public URL.
-  if (path === "/manual" || path === "/manual/") {
-    return Response.redirect(`${url.origin}/developer-guide.html`, 301);
-  }
-  if (path === "/developer-guide.html") {
-    const guideUrl = new URL(url);
-    guideUrl.pathname = "/developer-guide-content/";
-    return context.next(new Request(guideUrl.toString(), context.request));
-  }
-
   // Let static assets and _redirects run first.
   const response = await context.next();
   if (response.status !== 404) {
