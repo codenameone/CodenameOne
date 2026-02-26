75940b9946036bcca0c0a3f57f4d512049dd4809
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2026-02-26 16:03:30 +0200

    ImageViewer: added optional navigation arrows and thumbnail strip  (#4571)
    Fixed #3922

--- scripts/hellocodenameone/common/src/main/java/com/codenameone/examples/hellocodenameone/tests/OrientationLockScreenshotTest.java
+++ scripts/hellocodenameone/common/src/main/java/com/codenameone/examples/hellocodenameone/tests/OrientationLockScreenshotTest.java
@@ -1,6 +1,5 @@
 package com.codenameone.examples.hellocodenameone.tests;
 
-import com.codename1.testing.TestUtils;
 import com.codename1.ui.CN;
 import com.codename1.ui.Form;
 import com.codename1.ui.Label;
@@ -8,17 +7,36 @@ import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.util.UITimer;
 
 public class OrientationLockScreenshotTest extends BaseTest {
+    private static final int ORIENTATION_POLL_INTERVAL_MS = 100;
+    private static final int ORIENTATION_POLL_ATTEMPTS = 25;
+
     @Override
-    public boolean runTest() throws Exception {
-        Form hi = createForm("Orientation Lock", new BoxLayout(BoxLayout.Y_AXIS), "landscape");
+    public boolean runTest() {
+        Form hi = new Form("Orientation Lock", new BoxLayout(BoxLayout.Y_AXIS)) {
+            @Override
+            protected void onShowCompleted() {
+                CN.lockOrientation(false);
+                UITimer.timer(300, false, this, () -> {
+                    Cn1ssDeviceRunnerHelper.emitCurrentFormScreenshot("landscape");
+                    CN.lockOrientation(true);
+                    waitForOrientation(this, true, OrientationLockScreenshotTest.this::done);
+                });
+            }
+        };
         hi.add(new Label("Testing orientation lock..."));
-
         hi.show();
-
-        CN.lockOrientation(false);
-
-        TestUtils.waitFor(250);
-
         return true;
+    }
+    
+    private void waitForOrientation(Form form, boolean portrait, Runnable onDone) {
+        waitForOrientation(form, portrait, ORIENTATION_POLL_ATTEMPTS, onDone);
+    }
+
+    private void waitForOrientation(Form form, boolean portrait, int attemptsLeft, Runnable onDone) {
+        if (CN.isPortrait() == portrait || attemptsLeft <= 0) {
+            onDone.run();
+            return;
+        }
+        UITimer.timer(ORIENTATION_POLL_INTERVAL_MS, false, form, () -> waitForOrientation(form, portrait, attemptsLeft - 1, onDone));
     }
 }
