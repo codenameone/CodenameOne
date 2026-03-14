57246b717fd85fe97317b7832a0a4933023b697d
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2026-03-14 15:03:07 +0200

    Cleaned up failure with the Java 17 test

--- scripts/hellocodenameone/common/src/main/java/com/codenameone/examples/hellocodenameone/tests/Java17Tests.java
+++ scripts/hellocodenameone/common/src/main/java/com/codenameone/examples/hellocodenameone/tests/Java17Tests.java
@@ -29,30 +29,34 @@ public class Java17Tests extends BaseTest {
 
     @Override
     public boolean runTest() throws Exception {
-        var greeting = "Hello";
-        var target = "Codename One";
-        var record = new MyRecord(2, "V");
+        try {
+            var greeting = "Hello";
+            var target = "Codename One";
+            var record = new MyRecord(2, "V");
 
-        var message = switch (greeting.length()) {
-            case 5 -> greeting + " " + target;
-            default -> "unexpected";
-        };
+            var message = switch (greeting.length()) {
+                case 5 -> greeting + " " + target;
+                default -> "unexpected";
+            };
 
-        var textBlock = """
-                Java 17 language features
-                should compile in tests.
-                """;
+            var textBlock = """
+                    Java 17 language features
+                    should compile in tests.
+                    """;
 
-        assertEqual("Hello Codename One", message);
-        assertEqual("Java 17 language features\nshould compile in tests.\n", textBlock);
-        assertEqual(2, record.val());
-        assertEqual("V", record.otherVal());
-        assertTrue(record instanceof java.lang.Record);
-        assertTrue(record.getClass().isRecord());
-        assertEqual("MyRecord[val=2, otherVal=V]", record.toString());
-        assertEqual(148, record.hashCode());
-        assertTrue(record.equals(new MyRecord(2, "V")));
-        done();
-        return true;
+            assertEqual("Hello Codename One", message);
+            assertEqual("Java 17 language features\nshould compile in tests.\n", textBlock);
+            assertEqual(2, record.val());
+            assertEqual("V", record.otherVal());
+            assertEqual("MyRecord[val=2, otherVal=V]", record.toString());
+            assertEqual(148, record.hashCode());
+            assertTrue(record.equals(new MyRecord(2, "V")));
+            return true;
+        } catch (Throwable t) {
+            fail(String.valueOf(t));
+            return false;
+        } finally {
+            done();
+        }
     }
 }
