395edcf061209e7c43e7430a00d479a90d4a3dd9
liannacasper <67953602+liannacasper@users.noreply.github.com>
2026-04-07 18:21:45 +0300

    Optimize Base64 fast-path encode/decode loops

--- CodenameOne/src/com/codename1/util/Base64.java
+++ CodenameOne/src/com/codename1/util/Base64.java
@@ -155,6 +155,9 @@ public abstract class Base64 {
     }
 
     private static byte[] decodeNoWhitespace(byte[] in, int len) {
+        if ((len & 0x3) != 0) {
+            return null;
+        }
         int pad = 0;
         if (len > 0 && in[len - 1] == '=') {
             pad++;
@@ -172,43 +175,19 @@ public abstract class Base64 {
         }
         byte[] out = new byte[outLength];
         int outIndex = 0;
+        byte[] decodeMapLocal = decodeMap;
+        int fullLen = len - (pad > 0 ? 4 : 0);
 
-        int lastBlock = len - 4;
-        for (int i = 0; i < len; i += 4) {
+        for (int i = 0; i < fullLen; i += 4) {
             int c0 = in[i] & 0xff;
             int c1 = in[i + 1] & 0xff;
             int c2 = in[i + 2] & 0xff;
             int c3 = in[i + 3] & 0xff;
-            int b0 = decodeMap[c0];
-            int b1 = decodeMap[c1];
-            if (b0 < 0 || b1 < 0) {
-                return null;
-            }
-
-            if (i == lastBlock && pad > 0) {
-                if (pad == 2) {
-                    if (c2 != '=' || c3 != '=') {
-                        return null;
-                    }
-                    out[outIndex++] = (byte) ((b0 << 2) | (b1 >> 4));
-                    break;
-                } else {
-                    if (c3 != '=') {
-                        return null;
-                    }
-                    int b2 = decodeMap[c2];
-                    if (b2 < 0) {
-                        return null;
-                    }
-                    out[outIndex++] = (byte) ((b0 << 2) | (b1 >> 4));
-                    out[outIndex++] = (byte) ((b1 << 4) | (b2 >> 2));
-                    break;
-                }
-            }
-
-            int b2 = decodeMap[c2];
-            int b3 = decodeMap[c3];
-            if (b2 < 0 || b3 < 0) {
+            int b0 = decodeMapLocal[c0];
+            int b1 = decodeMapLocal[c1];
+            int b2 = decodeMapLocal[c2];
+            int b3 = decodeMapLocal[c3];
+            if ((b0 | b1 | b2 | b3) < 0) {
                 return null;
             }
             int quantum = (b0 << 18) | (b1 << 12) | (b2 << 6) | b3;
@@ -216,6 +195,32 @@ public abstract class Base64 {
             out[outIndex++] = (byte) ((quantum >> 8) & 0xff);
             out[outIndex++] = (byte) (quantum & 0xff);
         }
+
+        if (pad == 0) {
+            return out;
+        }
+
+        int i = len - 4;
+        int c0 = in[i] & 0xff;
+        int c1 = in[i + 1] & 0xff;
+        int b0 = decodeMapLocal[c0];
+        int b1 = decodeMapLocal[c1];
+        if ((b0 | b1) < 0) {
+            return null;
+        }
+        out[outIndex++] = (byte) ((b0 << 2) | (b1 >> 4));
+        if (pad == 2) {
+            return (in[i + 2] == '=' && in[i + 3] == '=') ? out : null;
+        }
+
+        if (in[i + 3] != '=') {
+            return null;
+        }
+        int b2 = decodeMapLocal[in[i + 2] & 0xff];
+        if (b2 < 0) {
+            return null;
+        }
+        out[outIndex] = (byte) ((b1 << 4) | (b2 >> 2));
         return out;
     }
 
@@ -289,7 +294,43 @@ public abstract class Base64 {
         byte[] mapLocal = map;
         int end = inputLength - (inputLength % 3);
         int outIndex = 0;
-        for (int i = 0; i < end; i += 3) {
+        int i = 0;
+        int fastEnd = end - 12;
+        for (; i <= fastEnd; i += 12) {
+            int b0 = in[i] & 0xff;
+            int b1 = in[i + 1] & 0xff;
+            int b2 = in[i + 2] & 0xff;
+            int b3 = in[i + 3] & 0xff;
+            int b4 = in[i + 4] & 0xff;
+            int b5 = in[i + 5] & 0xff;
+            int b6 = in[i + 6] & 0xff;
+            int b7 = in[i + 7] & 0xff;
+            int b8 = in[i + 8] & 0xff;
+            int b9 = in[i + 9] & 0xff;
+            int b10 = in[i + 10] & 0xff;
+            int b11 = in[i + 11] & 0xff;
+
+            out[outIndex++] = mapLocal[b0 >> 2];
+            out[outIndex++] = mapLocal[((b0 & 0x03) << 4) | (b1 >> 4)];
+            out[outIndex++] = mapLocal[((b1 & 0x0f) << 2) | (b2 >> 6)];
+            out[outIndex++] = mapLocal[b2 & 0x3f];
+
+            out[outIndex++] = mapLocal[b3 >> 2];
+            out[outIndex++] = mapLocal[((b3 & 0x03) << 4) | (b4 >> 4)];
+            out[outIndex++] = mapLocal[((b4 & 0x0f) << 2) | (b5 >> 6)];
+            out[outIndex++] = mapLocal[b5 & 0x3f];
+
+            out[outIndex++] = mapLocal[b6 >> 2];
+            out[outIndex++] = mapLocal[((b6 & 0x03) << 4) | (b7 >> 4)];
+            out[outIndex++] = mapLocal[((b7 & 0x0f) << 2) | (b8 >> 6)];
+            out[outIndex++] = mapLocal[b8 & 0x3f];
+
+            out[outIndex++] = mapLocal[b9 >> 2];
+            out[outIndex++] = mapLocal[((b9 & 0x03) << 4) | (b10 >> 4)];
+            out[outIndex++] = mapLocal[((b10 & 0x0f) << 2) | (b11 >> 6)];
+            out[outIndex++] = mapLocal[b11 & 0x3f];
+        }
+        for (; i < end; i += 3) {
             int b0 = in[i] & 0xff;
             int b1 = in[i + 1] & 0xff;
             int b2 = in[i + 2] & 0xff;
