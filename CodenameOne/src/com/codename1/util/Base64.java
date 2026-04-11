ace255d4b29b8931f67d52e319a7443676628c89
liannacasper <67953602+liannacasper@users.noreply.github.com>
2026-04-11 07:22:55 +0300

    Revert Base64 annotation opt-in after no benchmark gain

--- CodenameOne/src/com/codename1/util/Base64.java
+++ CodenameOne/src/com/codename1/util/Base64.java
@@ -19,8 +19,6 @@
 
 package com.codename1.util;
 
-import com.codename1.annotations.DisableDebugInfo;
-import com.codename1.annotations.DisableNullChecksAndArrayBoundsChecks;
 
 /// This class implements Base64 encoding/decoding functionality
 /// as specified in RFC 2045 (http://www.ietf.org/rfc/rfc2045.txt).
@@ -92,8 +90,6 @@ public abstract class Base64 {
      * @param out destination buffer
      * @return decoded length, or {@code -1} for invalid Base64
      */
-    @DisableDebugInfo
-    @DisableNullChecksAndArrayBoundsChecks
     public static int decode(byte[] in, int len, byte[] out) {
         if (len == 0) {
             return 0;
@@ -189,8 +185,6 @@ public abstract class Base64 {
         return decode(in, in.length, out);
     }
 
-    @DisableDebugInfo
-    @DisableNullChecksAndArrayBoundsChecks
     private static int decodeNoWhitespace(byte[] in, int len, byte[] out) {
         if ((len & 0x3) != 0) {
             return -1;
@@ -341,8 +335,6 @@ public abstract class Base64 {
      * @param out destination buffer
      * @return number of bytes written to {@code out}
      */
-    @DisableDebugInfo
-    @DisableNullChecksAndArrayBoundsChecks
     public static int encodeNoNewline(byte[] in, byte[] out) {
         int inputLength = in.length;
         int outputLength = ((inputLength + 2) / 3) * 4;
