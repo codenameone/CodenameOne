7487a8d36ad3a5ee69fe3296b6a13848d5a0e94b
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2026-07-11 18:49:14 +0300

    Clean before generating local iOS Xcode projects

--- maven/codenameone-maven-plugin/src/main/java/com/codename1/maven/buildWrappers/BuildIosXcodeProjectMojo.java
+++ maven/codenameone-maven-plugin/src/main/java/com/codename1/maven/buildWrappers/BuildIosXcodeProjectMojo.java
@@ -4,6 +4,9 @@ package com.codename1.maven.buildWrappers;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Mojo(name="buildIosXcodeProject", requiresDependencyResolution = ResolutionScope.NONE,
         requiresDependencyCollection = ResolutionScope.NONE)
 public class BuildIosXcodeProjectMojo extends AbstractBuildWrapperMojo {
@@ -15,5 +18,12 @@ public class BuildIosXcodeProjectMojo extends AbstractBuildWrapperMojo {
     @Override
     protected String getBuildTarget() {
         return "ios-source";
+    }
+
+    @Override
+    protected List<String> getGoals() {
+        // Incremental native source generation can leave an Xcode project with
+        // stale VM output.  Always regenerate it from a clean Maven build.
+        return Arrays.asList("clean", "package");
     }
 }
