c4a21289c80a6da1af1b0dc9e7e3be857dba2949
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2026-07-15 07:26:35 +0300

    Update editor sources for copyright header gate

--- CodenameOne/src/com/codename1/ui/TextInputConfig.java
+++ CodenameOne/src/com/codename1/ui/TextInputConfig.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2026, Codename One and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
+ * published by the Free Software Foundation.  Codename One designates this
  * particular file as subject to the "Classpath" exception as provided
  * by Oracle in the LICENSE file that accompanied this code.
  *
@@ -17,10 +17,10 @@
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores
- * CA 94065 USA or visit www.oracle.com if you need additional information or
- * have any questions.
+ * Please contact Codename One through http://www.codenameone.com/ if you
+ * need additional information or have any questions.
  */
+
 package com.codename1.ui;
 
 /// Immutable configuration handed to the platform when a `TextInputClient` binds to the low level text
