a24869624fc5f805ffb03cf54a5f74a025e567f1
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2026-07-27 16:47:13 +0300

    Guard microphone recording on tvOS

--- Ports/iOSPort/nativeSources/IOSNative.m
+++ Ports/iOSPort/nativeSources/IOSNative.m
@@ -8118,7 +8118,7 @@ JAVA_BOOLEAN com_codename1_impl_ios_IOSNative_checkLocationUsage___R_boolean(CN1
 }
 //native boolean checkMicrophoneUsage();
 JAVA_BOOLEAN com_codename1_impl_ios_IOSNative_checkMicrophoneUsage___R_boolean(CN1_THREAD_STATE_MULTI_ARG JAVA_OBJECT me) {
-#ifdef INCLUDE_MICROPHONE_USAGE
+#if defined(INCLUDE_MICROPHONE_USAGE) && !TARGET_OS_TV
     return JAVA_TRUE;
 #else
     return JAVA_FALSE;
@@ -8584,7 +8584,7 @@ void com_codename1_impl_ios_IOSNative_nsDataToByteArray___long_byte_1ARRAY(CN1_T
 
 JAVA_LONG com_codename1_impl_ios_IOSNative_createAudioUnit___java_lang_String_int_float_float_1ARRAY_R_long(CN1_THREAD_STATE_MULTI_ARG JAVA_OBJECT instanceObject,
     JAVA_OBJECT path, JAVA_INT audioChannels, JAVA_FLOAT sampleRate, JAVA_OBJECT sampleBuffer) {
-#ifdef INCLUDE_MICROPHONE_USAGE
+#if defined(INCLUDE_MICROPHONE_USAGE) && !TARGET_OS_TV
         __block CN1AudioUnit* recorder = nil;
          
         __block NSString *exStr = nil;
@@ -8674,7 +8674,7 @@ void com_codename1_impl_ios_IOSNative_destroyAudioUnit___long(CN1_THREAD_STATE_M
 
 JAVA_LONG com_codename1_impl_ios_IOSNative_createAudioRecorder___java_lang_String_java_lang_String_int_int_int_int(CN1_THREAD_STATE_MULTI_ARG JAVA_OBJECT instanceObject,
                                                                                   JAVA_OBJECT  destinationFile, JAVA_OBJECT mimeType, JAVA_INT sampleRate, JAVA_INT bitRate, JAVA_INT channels, JAVA_INT maxDuration) {
-#ifdef INCLUDE_MICROPHONE_USAGE
+#if defined(INCLUDE_MICROPHONE_USAGE) && !TARGET_OS_TV
     __block AVAudioRecorder* recorder = nil;
      
     __block NSString *exStr = nil;
