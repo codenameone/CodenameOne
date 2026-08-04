c5daa9e41890debede9adcc593287a406c8cdf62
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2026-08-04 10:57:30 +0300

    scripts/website: bump Codename One versions to 7.0.264 (#5499)

--- scripts/initializr/common/src/main/java/com/codename1/initializr/model/GeneratorModel.java
+++ scripts/initializr/common/src/main/java/com/codename1/initializr/model/GeneratorModel.java
@@ -41,7 +41,7 @@ import java.util.Map;
 import static com.codename1.ui.CN.*;
 
 public class GeneratorModel {
-    private static final String CN1_PLUGIN_VERSION = "7.0.263";
+    private static final String CN1_PLUGIN_VERSION = "7.0.264";
     private static final String PREVIEW_BUTTON_SELECTOR =
             "Button, InitializrLiveButtonDarkClean, "
                     + "InitializrLiveButtonLightTealRound, InitializrLiveButtonLightTealSquare, "
